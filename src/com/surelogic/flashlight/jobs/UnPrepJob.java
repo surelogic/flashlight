57668175d736742610276d04ab8eac1124d0adf4
Edwin Chan <edwin.chan@surelogic.com>
2008-02-06 17:49:50 +0000

    Fixed package name
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@5673 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/jobs/UnPrepJob.java
+++ src/com/surelogic/flashlight/jobs/UnPrepJob.java
@@ -8,7 +8,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
-import com.surelogic.common.eclipse.job.DatabaseJob;
+import com.surelogic.common.eclipse.jobs.DatabaseJob;
 import com.surelogic.common.eclipse.logging.SLStatus;
 import com.surelogic.flashlight.Data;
 import com.surelogic.flashlight.views.RunView;

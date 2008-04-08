3a075c749a10690fb16a78b8f92edeabdc9a9d02
Edwin Chan <edwin.chan@surelogic.com>
2008-04-08 13:25:18 +0000

    Copied to flashlight-common
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@7222 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/common/Utility.java
@@ -0,0 +1,39 @@
+package com.surelogic.flashlight;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class Utility {
+
+	private Utility() {
+		// no instances
+	}
+
+	private final static ThreadLocal<SimpleDateFormat> tl_format = new ThreadLocal<SimpleDateFormat>() {
+		@Override
+		protected SimpleDateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+	};
+
+	public static synchronized String toStringMS(final Date date) {
+		return tl_format.get().format(date);
+	}
+
+	public static Timestamp getWall(final Timestamp start, final long startNS,
+			final long timeNS) {
+		long tMS = start.getTime();
+		final long deltaNS = timeNS - startNS;
+		if (deltaNS < 0)
+			throw new IllegalStateException("timeNS=" + timeNS
+					+ " cannot be less than startedNS=" + startNS);
+		final long deltaMS = deltaNS / 1000000;
+		tMS = tMS + deltaMS;
+		long tDecNS = (tMS % 1000) * 1000000;
+		tDecNS = tDecNS + (deltaNS % 1000000);
+		Timestamp result = new Timestamp(tMS);
+		result.setNanos((int) tDecNS);
+		return result;
+	}
+}

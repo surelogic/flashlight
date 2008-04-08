673a16c4560e889a1c62a3450d0770094cf78e58
Edwin Chan <edwin.chan@surelogic.com>
2008-04-08 13:25:10 +0000

    Copied to flashlight-common
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@7212 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/common/actions/RunToolbarAction.java
@@ -0,0 +1,10 @@
+package com.surelogic.flashlight.actions;
+
+import org.eclipse.debug.ui.actions.AbstractLaunchToolbarAction;
+
+public class RunToolbarAction extends AbstractLaunchToolbarAction {
+
+	public RunToolbarAction() {
+		super("com.surelogic.flashlight.launchGroup.flashlight");
+	}
+}

282f291ecbde22a988cdcf345b56e00090e0b6ee
Edwin Chan <edwin.chan@surelogic.com>
2008-04-14 21:07:09 +0000

    Refactored to use code in TestLockOrdering Added code to check reentrant behavior after a wait()
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@7378 d02ae147-ee12-4a35-9452-91c89e8b2592

--- test/com/surelogic/dynamic/test/TestReentrantLocks.java
+++ test/com/surelogic/dynamic/test/TestReentrantLocks.java
@@ -1,28 +1,43 @@
 package com.surelogic.dynamic.test;
 
 public class TestReentrantLocks {
-	static Object a = new Object();
-	static Object b = a;
+	
+    public static void main(String[] args) {	    	
+    	TestLockOrdering.b = TestLockOrdering.a;
+    	TestLockOrdering.ab();
+    	TestLockOrdering.ba();
+    	
+    	reenterAndWait();
+    }
 
-	public static void main(String[] args) {	
-		ab();
-		ba();
-	}
-
-	static void ab() {
-		synchronized (a) {
-			synchronized (b) {
-				System.out.println("ab");
+	private static synchronized void reenterAndWait() {
+		final Object lock = TestLockOrdering.class;
+		synchronized (lock) {
+			Thread t = new Thread() {
+				@Override public void run() {
+					try {
+						System.out.println("Sleeping");
+						sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+									
+					// Wake up the first thread				
+					synchronized (lock) {								
+						lock.notifyAll();
+						System.out.println("Done notifying threads");
+					}
+				}
+			};			
+			t.start();	
+			System.out.println("Thread started");
+			try {				
+				lock.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		}
-	}
-
-	static void ba() {
-		synchronized (b) {
-			synchronized (a) {
-				System.out.println("ba");
-			}
-		}
+			System.out.println("Done waiting");
+		}		
 	}
 }
 

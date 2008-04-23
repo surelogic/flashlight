5821d117c8f06f4b86f5f602ab4a7fe0c2f1b09d
Edwin Chan <edwin.chan@surelogic.com>
2008-04-23 14:44:49 +0000

    Changed to stress the garbage collector more
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@7513 d02ae147-ee12-4a35-9452-91c89e8b2592

--- test/com/surelogic/dynamic/test/TestGarbageCollectedObjects.java
+++ test/com/surelogic/dynamic/test/TestGarbageCollectedObjects.java
@@ -4,20 +4,31 @@ import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TestGarbageCollectedObjects {
-    public static void main(String[] args) {
+    static final int MB = 1024*1024;
+	
+	public static void main(String[] args) {
     	final Queue q = new LinkedBlockingQueue();
     	new Thread() {
     		@Override public void run() {
-    			for(int i=0; i<1000; i++) {    				
-    				q.offer(Integer.toString(i));
+    			for(int i=0; i<1000; i++) {    	
+    				try {
+    					q.offer(Integer.toString(i));
+    					q.offer(new byte[MB]);
+    				} catch (OutOfMemoryError e) {
+    					System.gc();
+    				}
     			}
     		}
     	}.start();
     	new Thread() {
     		@Override public void run() {
     			for(int i=0; i<1000; i++) {
-    				System.out.println(q.poll());
-    				System.gc();
+    				Object o = q.poll();
+    				if (o != null) {
+    					System.out.println(o);
+    					q.poll();
+    					System.gc();
+    				}
     			}
     		}
     	}.start();

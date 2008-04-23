b4d09d13a49085953fe57280b7adb7f6046fccaf
Edwin Chan <edwin.chan@surelogic.com>
2008-04-23 19:28:17 +0000

    Changed to be shorter
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@7538 d02ae147-ee12-4a35-9452-91c89e8b2592

--- test/com/surelogic/dynamic/test/TestGarbageCollectedObjects.java
+++ test/com/surelogic/dynamic/test/TestGarbageCollectedObjects.java
@@ -18,7 +18,7 @@ public class TestGarbageCollectedObjects {
     	final Queue q = new LinkedBlockingQueue();
     	new Thread() {
     		@Override public void run() {
-    			for(int i=0; i<1000; i++) {    	
+    			for(int i=0; i<100; i++) {    	
     				try {
     					q.offer(new Holder(i));
     					byte[] buf = new byte[MB];
@@ -32,7 +32,7 @@ public class TestGarbageCollectedObjects {
     	}.start();
     	new Thread() {
     		@Override public void run() {
-    			for(int i=0; i<2000; i++) {
+    			for(int i=0; i<200; i++) {
     				Object o = q.poll();
     				if (o != null) {
     					if (o instanceof byte[]) {

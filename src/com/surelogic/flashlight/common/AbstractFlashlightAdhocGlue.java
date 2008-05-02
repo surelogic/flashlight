634650149a7b342254e2f7519e272b4a44e20936
Edwin Chan <edwin.chan@surelogic.com>
2008-05-02 15:35:03 +0000

    Refactored glue code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@7727 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/common/AbstractFlashlightAdhocGlue.java
@@ -0,0 +1,32 @@
+package com.surelogic.flashlight.common;
+
+import java.io.*;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.surelogic.adhoc.AbstractAdHocGlue;
+
+public abstract class AbstractFlashlightAdhocGlue extends AbstractAdHocGlue {
+	public final Connection getConnection() throws SQLException {
+		return Data.getConnection();
+	}
+	
+    protected static void copyDefaultQueryFile(File qsf) throws IOException {
+        InputStream is = null;
+        try {
+            URL url = Data.getDefaultQueryFileURL();
+            is = url.openStream();
+            is = new BufferedInputStream(is, 8192);
+            OutputStream os = new FileOutputStream(qsf);
+            
+            byte[] buf = new byte[8192];
+            int num;
+            while ((num = is.read(buf)) >= 0) {
+                os.write(buf, 0, num);
+            }
+        } finally {
+            if (is != null) is.close();
+        }
+    }
+}

942a075fcb58e5a8b4a1cb52cc4bdd527d9739eb
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2008-06-18 20:21:09 +0000

    Added more final modifiers to parameters
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@8407 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/rewriter/test/DebugStore.java
+++ src/com/surelogic/_flashlight/rewriter/test/DebugStore.java
@@ -1,11 +1,10 @@
-package com.surelogic._flashlight.rewriter.test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 
 public class DebugStore {
   public static synchronized void fieldAccess(final boolean read, final Object receiver,
-      final Field field, Class<?> withinClass, final int line) {
+      final Field field, final Class<?> withinClass, final int line) {
     System.out.println("fieldAccess");
     System.out.println("  read = " + read);
     System.out.println("  receiver = " + receiver);
@@ -17,7 +16,7 @@ public class DebugStore {
 
   public static synchronized void beforeIntrinsicLockAcquisition(final Object lockObject,
       final boolean lockIsThis, final boolean lockIsClass,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("beforeIntrinsicLockAcquisition");
     System.out.println("  lockObject = " + lockObject);
     System.out.println("  lockIsThis = " + lockIsThis);
@@ -28,7 +27,7 @@ public class DebugStore {
   }
 
   public static synchronized void afterIntrinsicLockAcquisition(final Object lockObject,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("afterIntrinsicLockAcquisition");
     System.out.println("  lockObject = " + lockObject);
     System.out.println("  withinClass = " + withinClass);
@@ -37,7 +36,7 @@ public class DebugStore {
   }
   
   public static synchronized void afterIntrinsicLockRelease(final Object lockObject,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("afterIntrinsicLockRelease");
     System.out.println("  lockObject = " + lockObject);
     System.out.println("  withinClass = " + withinClass);
@@ -46,7 +45,7 @@ public class DebugStore {
   }
 
   public static synchronized void intrinsicLockWait(final boolean before,
-      final Object lockObject, Class<?> withinClass, final int line) {
+      final Object lockObject, final Class<?> withinClass, final int line) {
     System.out.println("intrinsicLockWait");
     System.out.println("  before = " + before);
     System.out.println("  lockObject = " + lockObject);
@@ -57,7 +56,7 @@ public class DebugStore {
   
   public static synchronized void methodCall(final boolean before, final Object receiver,
       final String enclosingFileName, final String enclosingLocationName,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("methodCall");
     System.out.println("  before = " + before);
     System.out.println("  receiver = " + receiver);
@@ -70,7 +69,7 @@ public class DebugStore {
 
   public static synchronized void constructorCall(final boolean before,
       final String enclosingFileName,
-      final String enclosingLocationName, Class<?> withinClass,
+      final String enclosingLocationName, final Class<?> withinClass,
       final int line) {
     System.out.println("constructorCall");
     System.out.println("  before = " + before);
@@ -82,7 +81,7 @@ public class DebugStore {
   }
   
   public static synchronized void constructorExecution(final boolean before,
-      final Object receiver, Class<?> withinClass, final int line) {
+      final Object receiver, final Class<?> withinClass, final int line) {
     System.out.println("constructorExecution");
     System.out.println("  before = " + before);
     System.out.println("  receiver = " + receiver);
@@ -92,7 +91,7 @@ public class DebugStore {
   }
 
   public static synchronized void beforeUtilConcurrentLockAcquisitionAttempt(
-      final Object lockObject, Class<?> withinClass, final int line) {
+      final Object lockObject, final Class<?> withinClass, final int line) {
     System.out.println("beforeUtilConcurrentLockAcquisitionAttempt");
     System.out.println("  lockObject = " + lockObject);
     System.out.println("  withinClass = " + withinClass);
@@ -102,7 +101,7 @@ public class DebugStore {
 
   public static synchronized void afterUtilConcurrentLockAcquisitionAttempt(
       final boolean gotTheLock, final Object lockObject,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("afterUtilConcurrentLockAcquisitionAttempt");
     System.out.println("  gotTheLock = " + gotTheLock);
     System.out.println("  lockObject = " + lockObject);
@@ -113,7 +112,7 @@ public class DebugStore {
 
   public static synchronized void afterUtilConcurrentLockReleaseAttempt(
       final boolean releasedTheLock, final Object lockObject,
-      Class<?> withinClass, final int line) {
+      final Class<?> withinClass, final int line) {
     System.out.println("afterUtilConcurrentLockReleaseAttempt");
     System.out.println("  releasedTheLock = " + releasedTheLock);
     System.out.println("  lockObject = " + lockObject);

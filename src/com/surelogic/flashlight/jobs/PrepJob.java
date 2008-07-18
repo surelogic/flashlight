d7d0529255c71af6d08e892cfa2e9932fbc3f74e
Tim Halloran <tim.halloran@surelogic.com>
2008-07-18 19:49:49 +0000

    Fixing SLStatus (for Eclipse)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@8796 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/jobs/PrepJob.java
+++ src/com/surelogic/flashlight/jobs/PrepJob.java
@@ -2,12 +2,13 @@ package com.surelogic.flashlight.jobs;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 
 import com.surelogic.common.eclipse.SLProgressMonitorWrapper;
 import com.surelogic.common.eclipse.jobs.DatabaseJob;
+import com.surelogic.common.eclipse.logging.SLStatusUtility;
+import com.surelogic.common.jobs.SLStatus;
 import com.surelogic.flashlight.common.files.Raw;
-import com.surelogic.flashlight.common.prep.*;
+import com.surelogic.flashlight.common.prep.PrepRunnable;
 import com.surelogic.flashlight.views.RunView;
 
 public final class PrepJob extends DatabaseJob {
@@ -21,16 +22,10 @@ public final class PrepJob extends DatabaseJob {
 
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
-		PrepRunnable prep = new PrepRunnable(f_raw, new SLProgressMonitorWrapper(monitor));
-		prep.run();
-		if (prep.getStatus() != null) {
-		    return (IStatus) prep.getStatus(); 	
-		}		
-		if (monitor.isCanceled()) {
-			return Status.CANCEL_STATUS;
-		}
+		final PrepRunnable prep = new PrepRunnable(f_raw);
+		final SLStatus status = prep.run(new SLProgressMonitorWrapper(monitor));
 		RunView.refreshViewContents();
 		monitor.done();
-		return Status.OK_STATUS;
+		return SLStatusUtility.convert(status);
 	}
 }

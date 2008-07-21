04081e858c64ce05887e74471efe75df60f5dfae
Tim Halloran <tim.halloran@surelogic.com>
2008-07-21 15:06:42 +0000

    Fixed Javadoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@8819 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/entities/IRunDescription.java
+++ src/com/surelogic/flashlight/common/entities/IRunDescription.java
@@ -2,6 +2,10 @@ package com.surelogic.flashlight.common.entities;
 
 import java.sql.Timestamp;
 
+/**
+ * Defines a common interface to describe Flashlight run data. The data could be
+ * on the disk or prepared into the database.
+ */
 public interface IRunDescription {
 
 	String getName();
@@ -26,5 +30,18 @@ public interface IRunDescription {
 
 	Timestamp getStartTimeOfRun();
 
+	/**
+	 * Compares two run descriptions. This method only checks if the two
+	 * descriptions represent the same run, it does not consider if the
+	 * description is about the run data on the disk or the run data in the
+	 * database. Therefore, if the raw and prepared representations of the same
+	 * run are compared, {@code true} will be returned.
+	 * 
+	 * @param run
+	 *            the run to compare with this one.
+	 * @return {@code true} if this run represents the same run as this one,
+	 *         regardless of if it is on the disk or in the database, {@code
+	 *         false} otherwise.
+	 */
 	boolean isSameRun(IRunDescription run);
 }

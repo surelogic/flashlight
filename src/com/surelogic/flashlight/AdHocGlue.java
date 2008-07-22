2faa40b473482de0b20fb46355a53ba3c044859a
Tim Halloran <tim.halloran@surelogic.com>
2008-07-22 18:34:18 +0000

    Working on gettting VisualVM to work again.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@8847 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/AdHocGlue.java
+++ src/com/surelogic/flashlight/AdHocGlue.java
@@ -1,37 +1,12 @@
 package com.surelogic.flashlight;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.Executor;
-import java.util.logging.Level;
-
-import com.surelogic.common.FileUtility;
-import com.surelogic.common.logging.SLLogger;
 import com.surelogic.flashlight.common.AbstractFlashlightAdhocGlue;
 import com.surelogic.flashlight.preferences.PreferenceConstants;
 
 public final class AdHocGlue extends AbstractFlashlightAdhocGlue {
+
 	public int getMaxRowsPerQuery() {
 		return Activator.getDefault().getPluginPreferences().getInt(
 				PreferenceConstants.P_MAX_ROWS_PER_QUERY);
-	}
-
-	public File getQuerySaveFile() {
-		File qsf = new File(FileUtility.getFlashlightDataDirectory()
-				+ File.separator + "queries.xml");
-		if (!qsf.exists()) {
-			try {
-				copyDefaultQueryFile(qsf);
-			} catch (IOException e) {
-				SLLogger.getLogger().log(Level.SEVERE,
-						"Problem creating default query file", e);
-			}
-		}
-		return qsf;
-	}
-
-	public Executor getExecutor() {
-		// TODO
-		throw new UnsupportedOperationException("Not implemented yet");
 	}
 }

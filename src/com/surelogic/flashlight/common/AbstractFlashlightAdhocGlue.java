dc494796b07a0d658181d1eac413dc594022f768
Tim Halloran <tim.halloran@surelogic.com>
2008-07-22 18:34:31 +0000

    Working on gettting VisualVM to work again.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@8850 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/AbstractFlashlightAdhocGlue.java
+++ src/com/surelogic/flashlight/common/AbstractFlashlightAdhocGlue.java
@@ -1,33 +1,29 @@
 package com.surelogic.flashlight.common;
 
-import java.io.*;
-import java.net.URL;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Executor;
 
-import com.surelogic.adhoc.AbstractAdHocGlue;
+import com.surelogic.adhoc.IAdHoc;
+import com.surelogic.common.FileUtility;
 
-public abstract class AbstractFlashlightAdhocGlue extends AbstractAdHocGlue {
+public abstract class AbstractFlashlightAdhocGlue implements IAdHoc {
+
 	public final Connection getConnection() throws SQLException {
 		return Data.getInstance().getConnection();
 	}
-	
-    protected static void copyDefaultQueryFile(File qsf) throws IOException {
-        InputStream is = null;
-        try {
-            Data.getInstance();
-			URL url = Data.getDefaultQueryFileURL();
-            is = url.openStream();
-            is = new BufferedInputStream(is, 8192);
-            OutputStream os = new FileOutputStream(qsf);
-            
-            byte[] buf = new byte[8192];
-            int num;
-            while ((num = is.read(buf)) >= 0) {
-                os.write(buf, 0, num);
-            }
-        } finally {
-            if (is != null) is.close();
-        }
-    }
+
+	public File getQuerySaveFile() {
+		File qsf = new File(FileUtility.getFlashlightDataDirectory()
+				+ File.separator + "queries.xml");
+		if (!qsf.exists()) {
+			FileUtility.copy(Data.getDefaultQueryFileURL(), qsf);
+		}
+		return qsf;
+	}
+
+	public Executor getExecutor() {
+		return Data.getInstance().getExecutor();
+	}
 }

97cac531643eac9b2ac248a62b8f01822305613c
Tim Halloran <tim.halloran@surelogic.com>
2008-07-23 19:03:27 +0000

    Fixed packages in the Eclipse client
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@8914 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/common/jobs/RefreshRunManagerSLJob.java
@@ -0,0 +1,22 @@
+package com.surelogic.flashlight.common.jobs;
+
+import com.surelogic.common.SLProgressMonitor;
+import com.surelogic.common.jobs.SLJob;
+import com.surelogic.common.jobs.SLStatus;
+import com.surelogic.flashlight.common.model.RunManager;
+
+public final class RefreshRunManagerSLJob implements SLJob {
+
+	public SLStatus run(SLProgressMonitor monitor) {
+		if (monitor.isCanceled())
+			return SLStatus.CANCEL_STATUS;
+
+		try {
+			RunManager.getInstance().refresh();
+		} catch (Exception e) {
+			return SLStatus.createErrorStatus(SLStatus.OK,
+					"Refresh of run manager contents failed", e);
+		}
+		return SLStatus.OK_STATUS;
+	}
+}

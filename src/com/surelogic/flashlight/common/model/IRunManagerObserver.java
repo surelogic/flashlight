60d12a9ab7b8b7617dec413a69036f90e111fc07
Tim Halloran <tim.halloran@surelogic.com>
2008-07-24 13:32:27 +0000

    Improved Javadoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@8931 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/model/IRunManagerObserver.java
+++ src/com/surelogic/flashlight/common/model/IRunManagerObserver.java
@@ -6,6 +6,9 @@ public interface IRunManagerObserver {
 	 * Indicates that something about the set of runs being managed has changed.
 	 * This could include a new raw file being create, a raw file being
 	 * prepared, or data being deleted.
+	 * <p>
+	 * Implementors should note that this method is called from an unknown
+	 * thread.
 	 * 
 	 * @param manager
 	 *            the run manager.

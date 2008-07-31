c6a2b117fd8afbbb869c4e8a951a24da08d3b142
Tim Halloran <tim.halloran@surelogic.com>
2008-07-31 15:00:16 +0000

    Fixing use of progress monitor
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@9086 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/jobs/RefreshRunManagerSLJob.java
+++ src/com/surelogic/flashlight/common/jobs/RefreshRunManagerSLJob.java
@@ -11,12 +11,16 @@ public final class RefreshRunManagerSLJob implements SLJob {
 	public SLStatus run(SLProgressMonitor monitor) {
 		if (monitor == null)
 			monitor = new NullSLProgressMonitor();
+		monitor.beginTask("Refresh the Flashlight run manager contents",
+				SLProgressMonitor.UNKNOWN);
 
 		try {
 			RunManager.getInstance().refresh();
 		} catch (Exception e) {
 			return SLStatus.createErrorStatus(SLStatus.OK,
 					"Refresh of run manager contents failed", e);
+		} finally {
+			monitor.done();
 		}
 		return SLStatus.OK_STATUS;
 	}

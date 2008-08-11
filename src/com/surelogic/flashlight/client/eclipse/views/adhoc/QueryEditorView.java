f4f2a57ea019ae3c8c1312d61019ea5a0a451a8b
Tim Halloran <tim.halloran@surelogic.com>
2008-08-11 22:45:28 +0000

    Working on and IDE independent ad hoc query model.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9212 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryEditorView.java
@@ -0,0 +1,12 @@
+package com.surelogic.flashlight.client.eclipse.views.adhoc;
+
+import com.surelogic.adhoc.views.editor.AbstractQueryEditorView;
+import com.surelogic.common.adhoc.AdHocManager;
+
+public final class QueryEditorView extends AbstractQueryEditorView {
+
+	@Override
+	public AdHocManager getManager() {
+		return AdHocManager.getInstance(AdHocGlue.INSTANCE);
+	}
+}

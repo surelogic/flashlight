197d1c3f6cf0c881bb9cbbd4a07eb8e956f88557
Tim Halloran <tim.halloran@surelogic.com>
2008-08-14 22:53:00 +0000

    Working on and IDE independent ad hoc query model.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9308 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/perspectives/FlashlightPerspectiveFactory.java
+++ src/com/surelogic/flashlight/client/eclipse/perspectives/FlashlightPerspectiveFactory.java
@@ -4,6 +4,9 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import com.surelogic.flashlight.client.eclipse.views.adhoc.QueryEditorView;
+import com.surelogic.flashlight.client.eclipse.views.adhoc.QueryMenuView;
+import com.surelogic.flashlight.client.eclipse.views.adhoc.QueryResultsView;
 import com.surelogic.flashlight.client.eclipse.views.run.RunView;
 
 public final class FlashlightPerspectiveFactory implements IPerspectiveFactory {
@@ -12,8 +15,17 @@ public final class FlashlightPerspectiveFactory implements IPerspectiveFactory {
 		final String editorArea = layout.getEditorArea();
 		final String runViewArea = RunView.ID;
 
-		final IFolderLayout aboveEditorArea = layout.createFolder(
-				"aboveEditorArea", IPageLayout.TOP, 0.2f, editorArea);
-		aboveEditorArea.addView(runViewArea);
+		final IFolderLayout belowEditorArea = layout.createFolder(
+				"belowEditorArea", IPageLayout.BOTTOM, 0.5f, editorArea);
+		belowEditorArea.addView(QueryResultsView.class.getName());
+
+		final IFolderLayout leftOfEditorArea = layout.createFolder(
+				"leftOfEditorArea", IPageLayout.LEFT, 0.5f, editorArea);
+		leftOfEditorArea.addView(runViewArea);
+
+		final IFolderLayout rhsArea = layout.createFolder("rhsArea",
+				IPageLayout.BOTTOM, 0.4f, "leftOfEditorArea");
+		rhsArea.addView(QueryMenuView.class.getName());
+		rhsArea.addView(QueryEditorView.class.getName());
 	}
 }
---
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryMenuView.java
@@ -0,0 +1,13 @@
+package com.surelogic.flashlight.client.eclipse.views.adhoc;
+
+import com.surelogic.adhoc.views.menu.AbstractQueryMenuView;
+import com.surelogic.common.adhoc.AdHocManager;
+
+public final class QueryMenuView extends AbstractQueryMenuView {
+
+	@Override
+	public AdHocManager getManager() {
+		return FlashlightAdHocDataSource.getManager();
+	}
+
+}

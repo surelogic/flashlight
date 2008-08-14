17fa88424186a0a6adfa267ff5cedbb0e07c9058
Tim Halloran <tim.halloran@surelogic.com>
2008-08-14 19:38:06 +0000

    Working on and IDE independent ad hoc query model.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9296 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/views/adhoc/FlashlightAdHocDataSource.java
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/FlashlightAdHocDataSource.java
@@ -4,9 +4,9 @@ import com.surelogic.flashlight.client.eclipse.Activator;
 import com.surelogic.flashlight.client.eclipse.preferences.PreferenceConstants;
 import com.surelogic.flashlight.common.AbstractFlashlightAdhocDataSource;
 
-public final class AdHocDataSource extends AbstractFlashlightAdhocDataSource {
+public final class FlashlightAdHocDataSource extends AbstractFlashlightAdhocDataSource {
 
-	public static final AdHocDataSource INSTANCE = new AdHocDataSource();
+	public static final FlashlightAdHocDataSource INSTANCE = new FlashlightAdHocDataSource();
 
 	public int getMaxRowsPerQuery() {
 		return Activator.getDefault().getPluginPreferences().getInt(
--- src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryEditorView.java
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryEditorView.java
@@ -7,6 +7,6 @@ public final class QueryEditorView extends AbstractQueryEditorView {
 
 	@Override
 	public AdHocManager getManager() {
-		return AdHocManager.getInstance(AdHocDataSource.INSTANCE);
+		return AdHocManager.getInstance(FlashlightAdHocDataSource.INSTANCE);
 	}
 }

1233f791fa5f3ac6a4c1f62b4e5bd3f75c4402c4
Edwin Chan <edwin.chan@surelogic.com>
2008-09-11 14:31:17 +0000

    Added caching (no speedup right now)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@9540 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/SrcLoc.java
+++ src/com/surelogic/_flashlight/SrcLoc.java
@@ -1,11 +1,50 @@
 package com.surelogic._flashlight;
 
-final class SrcLoc {
+import java.util.concurrent.*;
 
+import com.surelogic._flashlight.jsr166y.ConcurrentReferenceHashMap;
+import com.surelogic._flashlight.jsr166y.ConcurrentReferenceHashMap.ReferenceType;
+
+final class SrcLoc {	
+	/**
+	 * Cache for SrcLoc objects
+	 */
+	/*
+	private static final ConcurrentMap<Class, ConcurrentHashMap<Integer, SrcLoc>> f_cache = 
+		new ConcurrentReferenceHashMap<Class, ConcurrentHashMap<Integer, SrcLoc>>(ReferenceType.WEAK, 
+				                                                                  ReferenceType.STRONG, true);
+	*/
+	
 	static final SrcLoc UNKNOWN = new SrcLoc(UnknownError.class, 0);
 
 	private final int f_line;
 
+	static SrcLoc getLocation(final Class<?> withinClass, final int line) {
+		/*
+		// Check if it's in the cache first
+		ConcurrentHashMap<Integer, SrcLoc> lineMap = f_cache.get(withinClass);
+		if (lineMap == null) {
+			ConcurrentHashMap<Integer, SrcLoc> temp = new ConcurrentHashMap<Integer, SrcLoc>();
+			lineMap = f_cache.putIfAbsent(withinClass, temp);
+			if (lineMap == null) {
+				lineMap = temp;
+			}
+		}
+		Integer key = Integer.valueOf(line);
+		SrcLoc loc  = lineMap.get(key);
+		if (loc != null) {
+			return loc;
+		}
+		SrcLoc temp = new SrcLoc(withinClass, line);
+		loc = lineMap.putIfAbsent(key, temp);
+		if (loc == null) {
+			loc = temp;
+		}
+		return loc;
+		*/
+		return new SrcLoc(withinClass, line);
+	}
+	
 	int getLine() {
 		return f_line;
 	}
@@ -27,4 +66,18 @@ final class SrcLoc {
 	public String toString() {
 		return f_withinClass + ":" + f_line;
 	}
-}
+	
+	@Override 
+	public int hashCode() {
+		return (int) (getWithinClassId() + f_line);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof SrcLoc) {
+			SrcLoc loc2 = (SrcLoc) o;
+			return f_line == loc2.f_line && f_withinClass == loc2.f_withinClass;
+		}
+		return false;
+	}
+} 

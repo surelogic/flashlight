016cabff8eb8b1c4d9c13cb61a897b023d74034d
Tim Halloran <tim.halloran@surelogic.com>
2008-09-18 17:40:16 +0000

    Working on jump to code in the Java editor
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9627 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/views/adhoc/JumpToCode.java
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/JumpToCode.java
@@ -6,6 +6,15 @@ import com.surelogic.common.adhoc.AdHocManagerAdapter;
 import com.surelogic.common.adhoc.AdHocQueryResultSqlData;
 import com.surelogic.common.eclipse.JDTUtility;
 
+/**
+ * This class helps Flashlight to jump to lines of code within the Eclipse IDE
+ * when a result row is selected in a query result.
+ * <p>
+ * This class is a singleton.
+ * 
+ * @see AdHocDataSource#init()
+ * @see AdHocDataSource#dispose()
+ */
 public final class JumpToCode extends AdHocManagerAdapter {
 
 	private static final JumpToCode INSTANCE = new JumpToCode();
@@ -21,21 +30,35 @@ public final class JumpToCode extends AdHocManagerAdapter {
 	@Override
 	public void notifyResultVariableValueChange(AdHocQueryResultSqlData result) {
 		Map<String, String> variableValues = result.getVariableValues();
-		/*
-		 * Try to open an editor if the variables package, class, and line are
-		 * defined.
-		 */
 		final String packageName = variableValues.get("Package");
 		final String typeName = variableValues.get("Class");
-		final String line = variableValues.get("Line");
-		if (packageName != null && typeName != null && line != null) {
-			int lineNumber = 0;
-			try {
-				lineNumber = Integer.parseInt(line);
-			} catch (NumberFormatException e) {
-				// couldn't convert the line number so just use 0
+		if (packageName != null && typeName != null) {
+			final String line = variableValues.get("Line");
+			/*
+			 * Try to open an editor if the variables package, class, and line
+			 * are defined.
+			 */
+			if (line != null) {
+				int lineNumber = 0;
+				try {
+					lineNumber = Integer.parseInt(line);
+				} catch (NumberFormatException e) {
+					// couldn't convert the line number so just use 0
+				}
+				if (JDTUtility.tryToOpenInEditor(packageName, typeName,
+						lineNumber))
+					return;
 			}
-			JDTUtility.tryToOpenInEditor(packageName, typeName, lineNumber);
+			/*
+			 * Try to open an editor if the variables package, class, and field
+			 * name are defined.
+			 */
+			String fieldName = variableValues.get("FieldName");
+			if (fieldName == null)
+				fieldName = variableValues.get("Field Name");
+			if (fieldName != null) {
+				JDTUtility.tryToOpenInEditor(packageName, typeName, fieldName);
+			}
 		}
 	}
 }

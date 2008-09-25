f46ab486851d4b36b1980385c7ee22effa9ab9c3
Tim Halloran <tim.halloran@surelogic.com>
2008-09-25 15:24:41 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9757 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/actions/SendProblemAction.java
+++ src/com/surelogic/flashlight/client/eclipse/actions/SendProblemAction.java
@@ -2,29 +2,32 @@ package com.surelogic.flashlight.client.eclipse.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import com.surelogic.common.eclipse.SWTUtility;
+import com.surelogic.common.eclipse.dialogs.SendProblemReportDialog;
 
 public final class SendProblemAction implements IWorkbenchWindowActionDelegate {
 
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		// nothing to do
 	}
 
 	public void init(IWorkbenchWindow window) {
-		// TODO Auto-generated method stub
-
+		// nothing to do
 	}
 
 	public void run(IAction action) {
-		// TODO Auto-generated method stub
-
+		final SendProblemReportDialog dialog = new SendProblemReportDialog(
+				SWTUtility.getShell());
+		if (dialog.open() == Window.OK) {
+			System.out.println("OK pressed on send problem report dialog...");
+		}
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-
+		// nothing to do
 	}
-
 }

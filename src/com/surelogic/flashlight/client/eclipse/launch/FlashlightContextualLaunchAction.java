73b9e3bf972e36d2da5f4c24adb034449abdad7c
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2008-10-03 14:53:48 +0000

    Added "Flashlight As" submenu to the context menu.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@9856 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/flashlight/client/eclipse/launch/FlashlightContextualLaunchAction.java
@@ -0,0 +1,12 @@
+package com.surelogic.flashlight.client.eclipse.launch;
+
+import org.eclipse.debug.ui.actions.ContextualLaunchAction;
+
+public final class FlashlightContextualLaunchAction extends ContextualLaunchAction {
+  /**
+   * Constructor
+   */
+  public FlashlightContextualLaunchAction() {
+    super("flashlight");
+  }
+}

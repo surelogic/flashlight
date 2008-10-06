9d8ee7159da38aec391d96986a823634194bb708
Edwin Chan <edwin.chan@surelogic.com>
2008-10-06 14:52:03 +0000

    Added more toggles
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@9869 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/common/IdConstants.java
+++ src/com/surelogic/_flashlight/common/IdConstants.java
@@ -1,6 +1,8 @@
 package com.surelogic._flashlight.common;
 
 public final class IdConstants {
+	public static final boolean outputBinary = false;
 	public static final boolean useTraceNodes = false;
+	public static final boolean useRefinery = true;
 	public static final long ILLEGAL_FIELD_ID = Long.MIN_VALUE;
 }

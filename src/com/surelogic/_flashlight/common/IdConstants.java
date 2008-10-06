1b6b44f1041b75b54cc71363302c19685470e4f1
Edwin Chan <edwin.chan@surelogic.com>
2008-10-06 15:30:57 +0000

    Added toggle to filter events
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@9875 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/common/IdConstants.java
+++ src/com/surelogic/_flashlight/common/IdConstants.java
@@ -4,5 +4,6 @@ public final class IdConstants {
 	public static final boolean outputBinary = false;
 	public static final boolean useTraceNodes = false;
 	public static final boolean useRefinery = true;
+	public static final boolean filterEvents = true;
 	public static final long ILLEGAL_FIELD_ID = Long.MIN_VALUE;
 }

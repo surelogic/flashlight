fdfe582aff30e01635d8323e1d2e06c24c2ae659
Edwin Chan <edwin.chan@surelogic.com>
2008-10-06 20:18:00 +0000

    Added toggle for keeping location info in traced events
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@9889 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/common/IdConstants.java
+++ src/com/surelogic/_flashlight/common/IdConstants.java
@@ -3,6 +3,7 @@ package com.surelogic._flashlight.common;
 public final class IdConstants {
 	public static final boolean outputBinary = false;
 	public static final boolean useTraceNodes = false;
+	public static final boolean keepLocationInTracedEvents = true;
 	public static final boolean useRefinery = true;
 	public static final boolean filterEvents = true;
 	public static final long ILLEGAL_FIELD_ID = Long.MIN_VALUE;

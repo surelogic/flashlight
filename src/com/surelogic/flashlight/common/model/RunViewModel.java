db9033b045b1632b08c44610a046d186fc2e7199
Tim Halloran <tim.halloran@surelogic.com>
2008-10-08 19:21:42 +0000

    Working on license management
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@9937 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/model/RunViewModel.java
+++ src/com/surelogic/flashlight/common/model/RunViewModel.java
@@ -219,7 +219,7 @@ public final class RunViewModel {
 
 			@Override
 			String getText(RunDescription rowData) {
-				return SLUtility.toStringMS(rowData.getStartTimeOfRun());
+				return SLUtility.toStringHMS(rowData.getStartTimeOfRun());
 			}
 		});
 

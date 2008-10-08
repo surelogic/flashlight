5bdf3add8dba03b16f805fd4b5cbb7fae98a4271
Edwin Chan <edwin.chan@surelogic.com>
2008-10-08 13:38:40 +0000

    Changed to use later start time
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@9915 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/OutputStrategyBinary.java
+++ src/com/surelogic/_flashlight/OutputStrategyBinary.java
@@ -29,7 +29,6 @@ public class OutputStrategyBinary extends EventVisitor {
 	
 	public OutputStrategyBinary(ObjectOutputStream stream, Time time) {
 		f_out = stream;
-		start = time.getNanoTime();
 		try {
 			f_out.writeByte(First_Event.getByte());
 			f_out.writeUTF(version);
@@ -48,6 +47,7 @@ public class OutputStrategyBinary extends EventVisitor {
 		} catch (IOException ioe) {
 			handleIOException(ioe);
 		}	
+		start = System.nanoTime();
 	}
 
 	private static void addProperty(String key, ObjectOutputStream f_out) throws IOException {

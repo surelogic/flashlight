c3626d3e4bddd01e4bc2cb3dcf9800d860599fb2
Edwin Chan <edwin.chan@surelogic.com>
2008-10-10 17:45:56 +0000

    Copied Placeholder
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@9993 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/_flashlight/Placeholder.java
@@ -0,0 +1,86 @@
+package com.surelogic._flashlight.trace;
+
+import com.surelogic._flashlight.*;
+
+public class Placeholder implements ITraceNode {
+	final long f_siteId;
+	
+	final ITraceNode f_caller;
+	
+	Placeholder(long siteId, ITraceNode caller) {
+		f_siteId = siteId;
+		f_caller = caller;
+	}
+	
+	public TraceNode getNode(Store.State state) {
+		// First, try to see if I've cached a matching TraceNode
+		TraceNode caller;
+		ITraceNode callee;
+		if (f_caller != null) {			
+			// There's already a caller
+			caller = f_caller.getNode(state);			
+			callee = caller.getCallee(this.f_siteId);
+		} else {
+			// No caller yet
+			caller = null;
+			synchronized (TraceNode.roots) {	
+				callee = TraceNode.roots.get(this.f_siteId);	
+			}
+		}
+		if (callee != null) {
+			return callee.getNode(state);
+		}		
+		return TraceNode.newTraceNode(caller, f_siteId, state);
+	}
+	
+	public ITraceNode getCallee(long key) {
+		return null;
+	}
+	
+	public ITraceNode pushCallee(long siteId) {
+		// FIX
+		return new Placeholder(siteId, this);
+	}
+	
+	public ITraceNode popParent() {
+		// FIX
+		return f_caller;
+	}
+
+	public ITraceNode peekParent() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public final long getSiteId() {
+		return f_siteId;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int) f_siteId;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof ICallLocation) {
+			ICallLocation bt = (ICallLocation) o;
+            return bt.getSiteId() == getSiteId();
+ 		}
+		return false;
+	}
+	
+	public int getAndClearUnpropagated() {
+		return 0;
+	}
+	
+	public int addToUnpropagated(int count) {
+		return 0;
+	}
+
+	public static ITraceNode push(long siteId, ITraceNode caller) {
+		if (caller == null) {
+			return new Placeholder(siteId, caller);
+		}
+		return caller.pushCallee(siteId);
+	}
+}

fe8876dd4d039808917590efc2d0a8e44ac7d912
Tim Halloran <tim.halloran@surelogic.com>
2008-10-23 20:38:19 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@10187 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/jobs/SwitchToFlashlightPerspectiveJob.java
+++ src/com/surelogic/flashlight/client/eclipse/jobs/SwitchToFlashlightPerspectiveJob.java
@@ -3,17 +3,37 @@ package com.surelogic.flashlight.client.eclipse.jobs;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 
 import com.surelogic.common.eclipse.SWTUtility;
 import com.surelogic.common.eclipse.ViewUtility;
 import com.surelogic.common.eclipse.jobs.SLUIJob;
+import com.surelogic.flashlight.client.eclipse.Data;
 import com.surelogic.flashlight.client.eclipse.dialogs.ConfirmPerspectiveSwitch;
 import com.surelogic.flashlight.client.eclipse.perspectives.FlashlightPerspectiveFactory;
+import com.surelogic.flashlight.common.model.RunManager;
 
 public final class SwitchToFlashlightPerspectiveJob extends SLUIJob {
 
 	@Override
 	public IStatus runInUIThread(IProgressMonitor monitor) {
+		/*
+		 * First kick off a job to refresh the runs shown in the Flashlight Runs
+		 * view.
+		 */
+		final Job job = new Job("Refresh Runs") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				RunManager.getInstance().refresh(Data.getInstance());
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule();
+		/*
+		 * Now prompt the user to change to the Flashlight perspective, if we
+		 * are not already in it.
+		 */
 		final boolean change = ConfirmPerspectiveSwitch.toFlashlight(SWTUtility
 				.getShell());
 		if (change)

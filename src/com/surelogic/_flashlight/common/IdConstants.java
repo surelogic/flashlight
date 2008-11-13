84c2a7c9fc0428ebbf6ca61a9733b74e816b85df
Edwin Chan <edwin.chan@surelogic.com>
2008-11-13 20:46:14 +0000

    Set to factor out locks and threads for binary output
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@10493 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/common/IdConstants.java
+++ src/com/surelogic/_flashlight/common/IdConstants.java
@@ -6,8 +6,11 @@ public final class IdConstants {
 	public static final boolean useFieldAccesses = true;
 	public static final boolean keepLocationInTracedEvents = true;
 	public static final boolean filterEvents = true;
-	public static final boolean factorOutThread = false;
-	public static final boolean factorOutLock = false;
+	
+	// These next two only affect binary output
+	public static final boolean factorOutThread = true;
+	public static final boolean factorOutLock = true;
+	
 	public static final boolean writeOutput = true;
 	public static final boolean trackLocks = false;
 	public static final long ILLEGAL_ID = Long.MIN_VALUE;

fc1bfce7c03079e3523b5f7c5e61830f86f977c0
Edwin Chan <edwin.chan@surelogic.com>
2008-11-13 20:23:25 +0000

    Added command-line options to turn the refinery off, as well as to select the output type
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@10492 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/common/IdConstants.java
+++ src/com/surelogic/_flashlight/common/IdConstants.java
@@ -4,9 +4,7 @@ public final class IdConstants {
 	public static final boolean enableFlashlightToggle = false;
 	public static final boolean useTraces = true;
 	public static final boolean useFieldAccesses = true;
-	public static final boolean outputBinary = false;
 	public static final boolean keepLocationInTracedEvents = true;
-	public static final boolean useRefinery = true;
 	public static final boolean filterEvents = true;
 	public static final boolean factorOutThread = false;
 	public static final boolean factorOutLock = false;

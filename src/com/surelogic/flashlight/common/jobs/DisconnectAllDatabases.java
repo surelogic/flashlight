9c0414c075506d2da642b6862e2ce89a89198eeb
Nathan Boy <nathan.boy@surelogic.com>
2008-11-17 19:23:51 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@10515 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/jobs/DisconnectAllDatabases.java
+++ src/com/surelogic/flashlight/common/jobs/DisconnectAllDatabases.java
@@ -3,6 +3,7 @@ package com.surelogic.flashlight.common.jobs;
 import com.surelogic.common.jobs.AbstractSLJob;
 import com.surelogic.common.jobs.SLProgressMonitor;
 import com.surelogic.common.jobs.SLStatus;
+import com.surelogic.flashlight.common.model.FlashlightDBConnection;
 
 /**
  * This job is used to disconnect all Flashlight databases under the data
@@ -14,12 +15,17 @@ public final class DisconnectAllDatabases extends AbstractSLJob {
 		super("Disconnect all Flashlight databases");
 	}
 
-	public SLStatus run(SLProgressMonitor monitor) {
+	public SLStatus run(final SLProgressMonitor monitor) {
 		monitor.begin();
 		try {
 			/*
 			 * Disconnect from all connected Flashlight databases.
 			 */
+			try {
+				FlashlightDBConnection.shutdownConnections();
+			} catch (final Exception e) {
+				return SLStatus.createErrorStatus(e);
+			}
 		} finally {
 			monitor.done();
 		}

894086f5d2f409406bf60405f4dfcfa99c4af1c2
Edwin Chan <edwin.chan@surelogic.com>
2008-11-26 15:44:12 +0000

    Factored out constants out of instrumentation for general use
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@10603 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/_flashlight/common/InstrumentationConstants.java
@@ -0,0 +1,40 @@
+package com.surelogic._flashlight.common;
+
+public final class InstrumentationConstants {
+	public static final String FL_OFF = "FL_OFF";
+
+	public static final String FL_DIR = "FL_DIR";
+	public static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
+
+	public static final String FL_RUN = "FL_RUN";
+	public static final String FL_RUN_DEFAULT = "flashlight";
+
+	public static final String FL_RAWQ_SIZE = "FL_RAWQ_SIZE";
+	public static final int FL_RAWQ_SIZE_DEFAULT = 16;
+
+	public static final String FL_OUTQ_SIZE = "FL_OUTQ_SIZE";
+	public static final int FL_OUTQ_SIZE_DEFAULT = 16;
+
+	public static final String FL_REFINERY_SIZE = "FL_REFINERY_SIZE";
+	public static final int FL_REFINERY_SIZE_DEFAULT = 250;
+
+	public static final String FL_NO_SPY = "FL_NO_SPY";
+
+	public static final String FL_CONSOLE_PORT = "FL_CONSOLE_PORT";
+	public static final int FL_CONSOLE_PORT_DEFAULT = 43524;
+
+	public static final String FL_FIELDS_FILE = "FL_FIELDS_FILE";
+
+	public static final String FL_SITES_FILE = "FL_SITES_FILE";
+
+	public static final String FL_FILTERS_FILE = "FL_FILTERS_FILE";
+
+	public static final String FL_DATE_OVERRIDE = "FL_DATE_OVERRIDE";
+
+	public static final String FL_REFINERY_OFF = "FL_REFINERY_OFF";
+
+	public static final String FL_OUTPUT_TYPE = "FL_OUTPUT_TYPE";
+	public static final OutputType FL_OUTPUT_TYPE_DEFAULT = OutputType.FL_GZ;
+
+	public static final String FL_DEBUG = "FL_DEBUG";
+}

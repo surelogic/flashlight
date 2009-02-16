77d6367b33343b13c4d04ae5c09ab6fc6b0bdbd1
Tim Halloran <tim.halloran@surelogic.com>
2009-02-16 21:10:47 +0000

    Working on prompt for perspective change
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@11559 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/preferences/PreferenceConstants.java
+++ src/com/surelogic/flashlight/client/eclipse/preferences/PreferenceConstants.java
@@ -6,32 +6,35 @@ import com.surelogic.flashlight.client.eclipse.Activator;
  * Constant definitions for plug-in preferences
  */
 public class PreferenceConstants {
-	public static final String P_FL_PREFIX = "com.surelogic.flashlight.";
-	
-	public static final String P_FILTER_PKG_PREFIX = P_FL_PREFIX+"filter.";
-	
-	public static final String P_USE_FILTERING = P_FL_PREFIX+"use.filtering";
-	
-	public static final String P_OUTPUT_TYPE = P_FL_PREFIX+"output.type";
-	
-	public static final String P_COMPRESS_OUTPUT = P_FL_PREFIX+"compress.output";
-	
-	public static final String P_USE_REFINERY = P_FL_PREFIX+"use.refinery";
-	
-	public static final String P_RAWQ_SIZE = P_FL_PREFIX+"rawq.size";
 
-	public static final String P_OUTQ_SIZE = P_FL_PREFIX+"outq.size";
+	private static final String PREFIX = "com.surelogic.flashlight.";
 
-	public static final String P_REFINERY_SIZE = P_FL_PREFIX+"refinery.size";
+	public static final String P_FILTER_PKG_PREFIX = PREFIX + "filter.";
 
-	public static final String P_USE_SPY = P_FL_PREFIX+"use.spy";
+	public static final String P_USE_FILTERING = PREFIX + "use.filtering";
 
-	public static final String P_CONSOLE_PORT = P_FL_PREFIX+"console.port";
+	public static final String P_OUTPUT_TYPE = PREFIX + "output.type";
 
-	public static final String P_MAX_ROWS_PER_QUERY = P_FL_PREFIX+"max.rows.per.query";
+	public static final String P_COMPRESS_OUTPUT = PREFIX + "compress.output";
 
-	public static final String P_PROMPT_PERSPECTIVE_SWITCH = P_FL_PREFIX+"perspective.switch.prompt";
-	
+	public static final String P_USE_REFINERY = PREFIX + "use.refinery";
+
+	public static final String P_RAWQ_SIZE = PREFIX + "rawq.size";
+
+	public static final String P_OUTQ_SIZE = PREFIX + "outq.size";
+
+	public static final String P_REFINERY_SIZE = PREFIX + "refinery.size";
+
+	public static final String P_USE_SPY = PREFIX + "use.spy";
+
+	public static final String P_CONSOLE_PORT = PREFIX + "console.port";
+
+	public static final String P_MAX_ROWS_PER_QUERY = PREFIX
+			+ "max.rows.per.query";
+
+	public static final String P_PROMPT_PERSPECTIVE_SWITCH = PREFIX
+			+ "perspective.switch.prompt";
+
 	public static boolean getPromptForPerspectiveSwitch() {
 		return Activator.getDefault().getPluginPreferences().getBoolean(
 				P_PROMPT_PERSPECTIVE_SWITCH);
@@ -42,7 +45,8 @@ public class PreferenceConstants {
 				P_PROMPT_PERSPECTIVE_SWITCH, value);
 	}
 
-	public static final String P_AUTO_PERSPECTIVE_SWITCH = "com.surelogic.flashlight.perspective.switch.auto";
+	public static final String P_AUTO_PERSPECTIVE_SWITCH = PREFIX
+			+ "perspective.switch.auto";
 
 	public static boolean getAutoPerspectiveSwitch() {
 		return Activator.getDefault().getPluginPreferences().getBoolean(
@@ -54,7 +58,8 @@ public class PreferenceConstants {
 				P_AUTO_PERSPECTIVE_SWITCH, value);
 	}
 
-	public static final String P_AUTO_INCREASE_HEAP_AT_LAUNCH = "com.surelogic.flashlight.auto.increase.heap.at.launch";
+	public static final String P_AUTO_INCREASE_HEAP_AT_LAUNCH = PREFIX
+			+ "auto.increase.heap.at.launch";
 
 	public static boolean getAutoIncreaseHeapAtLaunch() {
 		return Activator.getDefault().getPluginPreferences().getBoolean(

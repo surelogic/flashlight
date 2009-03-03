ef9e1579a6a8f4532b70bb6dc0bb0c0a423726fb
Edwin Chan <edwin.chan@surelogic.com>
2009-03-03 21:30:12 +0000

    Refactored to share code for switching perspectives
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@11770 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/dialogs/ConfirmPerspectiveSwitch.java
+++ src/com/surelogic/flashlight/client/eclipse/dialogs/ConfirmPerspectiveSwitch.java
@@ -1,16 +1,26 @@
 package com.surelogic.flashlight.client.eclipse.dialogs;
 
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 
-import com.surelogic.common.eclipse.SLImages;
-import com.surelogic.common.eclipse.dialogs.ConfirmPerspectiveSwitchDialog;
-import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.eclipse.dialogs.AbstractConfirmPerspectiveSwitch;
 import com.surelogic.common.CommonImages;
 import com.surelogic.flashlight.client.eclipse.preferences.PreferenceConstants;
 
-public final class ConfirmPerspectiveSwitch {
-
+public final class ConfirmPerspectiveSwitch extends AbstractConfirmPerspectiveSwitch {
+	private static final ConfirmPerspectiveSwitch prototype = new ConfirmPerspectiveSwitch();	
+	
+	private ConfirmPerspectiveSwitch() {
+		super(null, PreferenceConstants.prototype);
+	}
+	@Override
+	protected String getLogo() {
+		return CommonImages.IMG_FL_LOGO;
+	}
+	@Override
+	protected String getShortPrefix() {
+		return "flashlight.";
+	}
+	
 	/**
 	 * Checks if the Flashlight perspective should be opened.
 	 * 
@@ -20,22 +30,6 @@ public final class ConfirmPerspectiveSwitch {
 	 *         {@code false} otherwise.
 	 */
 	public static boolean toFlashlight(Shell shell) {
-		if (PreferenceConstants.getPromptForPerspectiveSwitch()) {
-			ConfirmPerspectiveSwitchDialog dialog = new ConfirmPerspectiveSwitchDialog(
-					shell,
-					SLImages.getImage(CommonImages.IMG_FL_LOGO),
-					I18N
-							.msg("common.confirm.perspective.switch.dialog.flashlight"));
-			final boolean result = dialog.open() == Window.OK;
-			final boolean rememberMyDecision = dialog.getRememberMyDecision();
-			if (rememberMyDecision) {
-				PreferenceConstants
-						.setPromptForPerspectiveSwitch(!rememberMyDecision);
-				PreferenceConstants.setAutoPerspectiveSwitch(result);
-			}
-			return result;
-		} else {
-			return PreferenceConstants.getAutoPerspectiveSwitch();
-		}
+		return prototype.toPerspective(shell);
 	}
 }
--- src/com/surelogic/flashlight/client/eclipse/preferences/PreferenceConstants.java
+++ src/com/surelogic/flashlight/client/eclipse/preferences/PreferenceConstants.java
@@ -1,11 +1,12 @@
 package com.surelogic.flashlight.client.eclipse.preferences;
 
+import com.surelogic.common.eclipse.preferences.IPreferenceConstants;
 import com.surelogic.flashlight.client.eclipse.Activator;
 
 /**
  * Constant definitions for plug-in preferences
  */
-public class PreferenceConstants {
+public class PreferenceConstants implements IPreferenceConstants {
 
 	private static final String PREFIX = "com.surelogic.flashlight.";
 
@@ -35,12 +36,12 @@ public class PreferenceConstants {
 	public static final String P_PROMPT_PERSPECTIVE_SWITCH = PREFIX
 			+ "perspective.switch.prompt";
 
-	public static boolean getPromptForPerspectiveSwitch() {
+	public boolean getPromptForPerspectiveSwitch() {
 		return Activator.getDefault().getPluginPreferences().getBoolean(
 				P_PROMPT_PERSPECTIVE_SWITCH);
 	}
 
-	public static void setPromptForPerspectiveSwitch(boolean value) {
+	public void setPromptForPerspectiveSwitch(boolean value) {
 		Activator.getDefault().getPluginPreferences().setValue(
 				P_PROMPT_PERSPECTIVE_SWITCH, value);
 	}
@@ -48,12 +49,12 @@ public class PreferenceConstants {
 	public static final String P_AUTO_PERSPECTIVE_SWITCH = PREFIX
 			+ "perspective.switch.auto";
 
-	public static boolean getAutoPerspectiveSwitch() {
+	public boolean getAutoPerspectiveSwitch() {
 		return Activator.getDefault().getPluginPreferences().getBoolean(
 				P_AUTO_PERSPECTIVE_SWITCH);
 	}
 
-	public static void setAutoPerspectiveSwitch(boolean value) {
+	public void setAutoPerspectiveSwitch(boolean value) {
 		Activator.getDefault().getPluginPreferences().setValue(
 				P_AUTO_PERSPECTIVE_SWITCH, value);
 	}
@@ -65,4 +66,10 @@ public class PreferenceConstants {
 		return Activator.getDefault().getPluginPreferences().getBoolean(
 				P_AUTO_INCREASE_HEAP_AT_LAUNCH);
 	}
+	
+	private PreferenceConstants() {
+		// Nothing to do
+	}
+	
+	public static final PreferenceConstants prototype = new PreferenceConstants();
 }

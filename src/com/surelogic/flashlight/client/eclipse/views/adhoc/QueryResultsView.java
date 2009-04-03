e7dbb6eaf43155205d4e940e2cd263eb00a2f0cb
Tim Halloran <tim.halloran@surelogic.com>
2009-04-03 19:52:14 +0000

    Reworked the entire controls of the ResultExplorer. Added next and previous query controls and made a menu to take the less used options off of the toolbar (like export and show SQL).
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@12065 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryResultsView.java
+++ src/com/surelogic/flashlight/client/eclipse/views/adhoc/QueryResultsView.java
@@ -3,6 +3,7 @@ package com.surelogic.flashlight.client.eclipse.views.adhoc;
 import org.eclipse.swt.widgets.Composite;
 
 import com.surelogic.adhoc.views.results.AbstractQueryResultsView;
+import com.surelogic.common.adhoc.AdHocManager;
 import com.surelogic.common.serviceability.UsageMeter;
 
 public final class QueryResultsView extends AbstractQueryResultsView {
@@ -11,5 +12,10 @@ public final class QueryResultsView extends AbstractQueryResultsView {
 	public void createPartControl(Composite parent) {
 		UsageMeter.getInstance().tickUse("Flashlight QueryResultsView opened");
 		super.createPartControl(parent);
+	}
+	
+	@Override
+	public AdHocManager getManager() {
+		return AdHocDataSource.getManager();
 	}
 }

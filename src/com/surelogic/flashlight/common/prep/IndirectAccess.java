f3548a828655704b14536efec89f361da7c68256
Nathan Boy <nathan.boy@surelogic.com>
2009-04-22 15:38:16 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@12245 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/common/prep/IndirectAccess.java
+++ src/com/surelogic/flashlight/common/prep/IndirectAccess.java
@@ -1,21 +1,25 @@
 package com.surelogic.flashlight.common.prep;
 
-import static com.surelogic._flashlight.common.AttributeType.*;
-import static com.surelogic._flashlight.common.IdConstants.*;
+import static com.surelogic._flashlight.common.AttributeType.RECEIVER;
+import static com.surelogic._flashlight.common.IdConstants.ILLEGAL_ID;
+import static com.surelogic._flashlight.common.IdConstants.ILLEGAL_RECEIVER_ID;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.logging.Level;
 
 import com.surelogic._flashlight.common.PreppedAttributes;
 import com.surelogic.common.logging.SLLogger;
 
 public final class IndirectAccess extends Event {
-	//private static final String f_psQ = "INSERT INTO ACCESS (TS,InThread,Trace,Field,RW,Receiver,UnderConstruction) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	private static final String f_psQ = "INSERT INTO INDIRECTACCESS (TS,InThread,Trace,Receiver) VALUES (?, ?, ?, ?)";
 
-	//private PreparedStatement f_ps;
-	
-	public IndirectAccess(IntrinsicLockDurationRowInserter i) {
-		super(i); // NEEDED?
+	private PreparedStatement f_ps;
+
+	public IndirectAccess(final IntrinsicLockDurationRowInserter i) {
+		super(i);
 	}
 
 	public String getXMLElementName() {
@@ -27,21 +31,21 @@ public final class IndirectAccess extends Event {
 			final long startNS, final ScanRawFilePreScan scanResults)
 			throws SQLException {
 		super.setup(c, start, startNS, scanResults);
-		//f_ps = c.prepareStatement(f_psQ);
-		//f_scanResults = scanResults;
+		f_ps = c.prepareStatement(f_psQ);
 	}
-	
-	public void parse(PreppedAttributes attributes) throws SQLException {
+
+	public void parse(final PreppedAttributes attributes) throws SQLException {
 		final long nanoTime = attributes.getEventTime();
 		final long inThread = attributes.getThreadId();
 		final long trace = attributes.getTraceId();
 		final long receiver = attributes.getLong(RECEIVER);
-		
-		if ((nanoTime == ILLEGAL_ID) || (inThread == ILLEGAL_ID) || 
-			(trace == ILLEGAL_ID) || (receiver == ILLEGAL_RECEIVER_ID)) {
+
+		if ((nanoTime == ILLEGAL_ID) || (inThread == ILLEGAL_ID)
+				|| (trace == ILLEGAL_ID) || (receiver == ILLEGAL_RECEIVER_ID)) {
 			SLLogger.getLogger().log(
 					Level.SEVERE,
-					"Missing nano-time, thread, site, or field in " + getXMLElementName());
+					"Missing nano-time, thread, site, or field in "
+							+ getXMLElementName());
 			return;
 		}
 		insert(nanoTime, inThread, trace, receiver);
@@ -51,16 +55,15 @@ public final class IndirectAccess extends Event {
 
 	private void insert(final long nanoTime, final long inThread,
 			final long trace, final long receiver) throws SQLException {
-//		int idx = 1;
-//		f_ps.setTimestamp(idx++, getTimestamp(nanoTime), now);
-//		f_ps.setLong(idx++, inThread);
-//		f_ps.setLong(idx++, trace);
-//		f_ps.setLong(idx++, receiver);
-	
+		int idx = 1;
+		f_ps.setTimestamp(idx++, getTimestamp(nanoTime), now);
+		f_ps.setLong(idx++, inThread);
+		f_ps.setLong(idx++, trace);
+		f_ps.setLong(idx++, receiver);
 		if (doInsert) {
-			//f_ps.addBatch();
+			f_ps.addBatch();
 			if (++count == 10000) {
-				//f_ps.executeBatch();
+				f_ps.executeBatch();
 				count = 0;
 			}
 		}
@@ -69,10 +72,10 @@ public final class IndirectAccess extends Event {
 	@Override
 	public void flush(final long endTime) throws SQLException {
 		if (count > 0) {
-			//f_ps.executeBatch();
+			f_ps.executeBatch();
 			count = 0;
 		}
-		//f_ps.close();
+		f_ps.close();
 		super.flush(endTime);
 	}
 }

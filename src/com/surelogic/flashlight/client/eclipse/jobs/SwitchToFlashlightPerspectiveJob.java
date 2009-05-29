17345cd1ab5088353855da22704c6045b1faacc1
Tim Halloran <tim.halloran@surelogic.com>
2009-05-29 19:12:44 +0000

    Fixed multiple prompting for changing to the Flashlight perspective.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@12354 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/jobs/SwitchToFlashlightPerspectiveJob.java
+++ src/com/surelogic/flashlight/client/eclipse/jobs/SwitchToFlashlightPerspectiveJob.java
@@ -5,9 +5,11 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 
+import com.surelogic.common.eclipse.EclipseUtility;
 import com.surelogic.common.eclipse.SWTUtility;
 import com.surelogic.common.eclipse.ViewUtility;
 import com.surelogic.common.eclipse.jobs.SLUIJob;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.flashlight.client.eclipse.dialogs.ConfirmPerspectiveSwitch;
 import com.surelogic.flashlight.client.eclipse.perspectives.FlashlightPerspective;
 import com.surelogic.flashlight.common.model.RunManager;
@@ -28,19 +30,43 @@ public final class SwitchToFlashlightPerspectiveJob extends SLUIJob {
 			}
 		};
 		job.schedule();
+
 		/*
-		 * Now prompt the user to change to the Flashlight perspective, if we
-		 * are not already in it.
+		 * Ensure that we are not already in the Flashlight perspective.
 		 */
 		final boolean inFlashlightPerspective = ViewUtility
 				.isPerspectiveOpen(FlashlightPerspective.class.getName());
-		if (!inFlashlightPerspective) {
-			final boolean change = ConfirmPerspectiveSwitch
-					.toFlashlight(SWTUtility.getShell());
-			if (change) {
-				ViewUtility.showPerspective(FlashlightPerspective.class
-						.getName());
-			}
+		SLLogger.getLogger().fine(
+				"[PromptToPrepAllRawData] inFlashlightPerspective = "
+						+ inFlashlightPerspective);
+		if (inFlashlightPerspective)
+			return Status.OK_STATUS; // bail
+
+		/*
+		 * Check that we are the only job of this type running. This is trying
+		 * to avoid double prompting the user to change to the Flashlight
+		 * perspective. It may not work in all cases but should eliminate most
+		 * of them.
+		 * 
+		 * In particular if the dialog is already up and the user exits another
+		 * instrumented program then that exit will trigger another instance of
+		 * this job to run. Without this check the user would get two prompts to
+		 * change to the Flashlight perspective.
+		 */
+		final boolean onlySwitchToFlashlightPerspectiveJobRunning = EclipseUtility
+				.getActiveJobCountOfType(SwitchToFlashlightPerspectiveJob.class) == 1;
+		SLLogger
+				.getLogger()
+				.fine(
+						"[SwitchToFlashlightPerspectiveJob] onlySwitchToFlashlightPerspectiveJobRunning = "
+								+ onlySwitchToFlashlightPerspectiveJobRunning);
+		if (!onlySwitchToFlashlightPerspectiveJobRunning)
+			return Status.OK_STATUS; // bail
+
+		final boolean change = ConfirmPerspectiveSwitch.toFlashlight(SWTUtility
+				.getShell());
+		if (change) {
+			ViewUtility.showPerspective(FlashlightPerspective.class.getName());
 		}
 		return Status.OK_STATUS;
 	}

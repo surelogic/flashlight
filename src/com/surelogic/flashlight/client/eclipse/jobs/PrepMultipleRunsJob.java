fe73c45fa8ef0fa745c8794fa74eeedc7955bd3e
Nathan Boy <nathan.boy@surelogic.com>
2009-09-25 14:38:29 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@12960 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/jobs/PrepMultipleRunsJob.java
+++ src/com/surelogic/flashlight/client/eclipse/jobs/PrepMultipleRunsJob.java
@@ -12,7 +12,9 @@ import org.eclipse.core.runtime.jobs.Job;
 import com.surelogic.common.SLUtility;
 import com.surelogic.common.eclipse.jobs.KeywordAccessRule;
 import com.surelogic.common.eclipse.jobs.SLProgressMonitorWrapper;
+import com.surelogic.common.eclipse.logging.SLEclipseStatusUtility;
 import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.jobs.SLStatus;
 import com.surelogic.flashlight.client.eclipse.preferences.PreferenceConstants;
 import com.surelogic.flashlight.common.jobs.JobConstants;
 import com.surelogic.flashlight.common.jobs.PrepSLJob;
@@ -34,9 +36,11 @@ public class PrepMultipleRunsJob extends Job {
 		for (final RunDescription rd : f_runs) {
 			final ISchedulingRule rule = KeywordAccessRule.getInstance(
 					JobConstants.PREP_KEY, rd.toIdentityString());
+			SLStatus status;
 			try {
 				man.beginRule(rule, monitor);
-				new PrepSLJob(rd, PreferenceConstants.getPrepObjectWindowSize())
+				status = new PrepSLJob(rd, PreferenceConstants
+						.getPrepObjectWindowSize())
 						.run(new SLProgressMonitorWrapper(monitor, getName()));
 			} finally {
 				man.endRule(rule);
@@ -49,6 +53,9 @@ public class PrepMultipleRunsJob extends Job {
 						monitor, getName()));
 			} finally {
 				man.endRule(rule2);
+			}
+			if (status != SLStatus.OK_STATUS) {
+				return SLEclipseStatusUtility.convert(status);
 			}
 		}
 		return Status.OK_STATUS;

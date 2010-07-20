71e5f627cb910661584d49badec5acc0e5be7e27
Tim Halloran <tim.halloran@surelogic.com>
2010-07-20 18:55:01 +0000

    Added creation of an HTML overview to the prep.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@15250 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/jobs/PopulateBrowserWithRunInformationJob.java
+++ src/com/surelogic/flashlight/client/eclipse/jobs/PopulateBrowserWithRunInformationJob.java
@@ -7,7 +7,6 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.ui.progress.UIJob;
 
-import com.surelogic.common.SLUtility;
 import com.surelogic.common.eclipse.jobs.SLUIJob;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.flashlight.common.model.RunDescription;
@@ -39,30 +38,17 @@ public final class PopulateBrowserWithRunInformationJob extends Job {
 	protected IStatus run(IProgressMonitor monitor) {
 		monitor.beginTask(getName(), IProgressMonitor.UNKNOWN);
 		try {
-			final StringBuilder b = new StringBuilder();
-			/*
-			 * TODO Add real page information/generation/read from a file here.
-			 */
-			b.append("<html><body>");
-			b.append("<h4>").append(f_runDescription.getName()).append(' ');
-			b.append(SLUtility
-					.toStringHMS(f_runDescription.getStartTimeOfRun()));
-			b.append("</h4>");
-
-			b
-					.append("This is a <a href=\".#run_query_1\">link</a> to nowhere.");
-
-			b.append("</body></html>");
+			final String indexHtmlContents = f_runDescription.getRunDirectory()
+					.getHtmlHandles().readIndexHtml();
 			/*
 			 * Update the browser if it is still on the screen. This needs to be
 			 * done in a UI job.
 			 */
-			final String htmlText = b.toString();
 			UIJob job = new SLUIJob() {
 				@Override
 				public IStatus runInUIThread(IProgressMonitor monitor) {
 					if (!f_browser.isDisposed())
-						f_browser.setText(htmlText);
+						f_browser.setText(indexHtmlContents);
 					// TODO Auto-generated method stub
 					return Status.OK_STATUS;
 				}

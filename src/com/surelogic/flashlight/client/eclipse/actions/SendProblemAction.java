b6aacee6852d8f2e532b248a1af9006d6b21375f
Tim Halloran <tim.halloran@surelogic.com>
2011-05-27 01:56:13 +0000

    Working on Send Problem Report and Send Tip dialogs to fix several user-reported limitations.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@17628 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/actions/SendProblemAction.java
+++ src/com/surelogic/flashlight/client/eclipse/actions/SendProblemAction.java
@@ -7,8 +7,8 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import com.surelogic.common.CommonImages;
 import com.surelogic.common.core.EclipseUtility;
-import com.surelogic.common.ui.EclipseUIUtility;
-import com.surelogic.common.ui.dialogs.SendProblemReportDialog;
+import com.surelogic.common.license.SLLicenseProduct;
+import com.surelogic.common.ui.serviceability.SendProblemReportWizard;
 import com.surelogic.flashlight.client.eclipse.Activator;
 
 public final class SendProblemAction implements IWorkbenchWindowActionDelegate {
@@ -22,7 +22,7 @@ public final class SendProblemAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
-		SendProblemReportDialog.open(EclipseUIUtility.getShell(), "Flashlight "
+		SendProblemReportWizard.open(SLLicenseProduct.FLASHLIGHT + " "
 				+ EclipseUtility.getVersion(Activator.getDefault()),
 				CommonImages.IMG_FL_LOGO);
 	}

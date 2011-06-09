2cc81fc5bb4cf6a0ce628e63728ca9f4683313bf
Nathan Boy <nathan.boy@surelogic.com>
2011-06-09 18:22:15 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-instrumentation-5.0@17758 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/_flashlight/UtilConcurrent.java
+++ src/com/surelogic/_flashlight/UtilConcurrent.java
@@ -1,7 +1,5 @@
 package com.surelogic._flashlight;
 
-import java.util.Collections;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReadWriteLock;
 
@@ -11,23 +9,23 @@ import java.util.concurrent.locks.ReadWriteLock;
  */
 public final class UtilConcurrent {
 
-	private final Set<IdPhantomReference> f_knownReadWriteLockIds = Collections
-			.newSetFromMap(new ConcurrentHashMap<IdPhantomReference, Boolean>());
+    private final ConcurrentHashMap<IdPhantomReference, Boolean> f_knownReadWriteLockIds = new ConcurrentHashMap<IdPhantomReference, Boolean>();
 
-	/**
-	 * Adds a ReadWriteLock reference to the set of known instances.
-	 * 
-	 * @param o
-	 *            the reference to add.
-	 * @return {@code true} if this set did not already contain the specified
-	 *         element
-	 */
-	public boolean addReadWriteLock(final IdPhantomReference o) {
-		return f_knownReadWriteLockIds.add(o);
-	}
+    /**
+     * Adds a ReadWriteLock reference to the set of known instances.
+     * 
+     * @param o
+     *            the reference to add.
+     * @return {@code true} if this set did not already contain the specified
+     *         element
+     */
+    public boolean addReadWriteLock(final IdPhantomReference o) {
+        Boolean exists = f_knownReadWriteLockIds.put(o, true);
+        return exists == null;
+    }
 
-	void remove(final IdPhantomReference o) {
-		f_knownReadWriteLockIds.remove(o);
-	}
+    void remove(final IdPhantomReference o) {
+        f_knownReadWriteLockIds.remove(o);
+    }
 
 }

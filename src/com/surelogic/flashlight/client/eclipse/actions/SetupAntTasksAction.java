1704610041837b0331d5d628048b9989b1802c9e
Tim Halloran <tim.halloran@surelogic.com>
2011-06-16 18:10:11 +0000

    Fixed saving the ant tasks into a directory.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-client-eclipse@17805 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/flashlight/client/eclipse/actions/SetupAntTasksAction.java
+++ src/com/surelogic/flashlight/client/eclipse/actions/SetupAntTasksAction.java
@@ -1,36 +1,64 @@
 package com.surelogic.flashlight.client.eclipse.actions;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-import com.surelogic.common.CommonImages;
+import com.surelogic.common.FileUtility;
+import com.surelogic.common.i18n.I18N;
+import com.surelogic.common.logging.SLLogger;
 import com.surelogic.common.ui.EclipseUIUtility;
-import com.surelogic.flashlight.client.eclipse.dialogs.SetupAntDialog;
+import com.surelogic.flashlight.common.LibResources;
 
 public class SetupAntTasksAction implements IWorkbenchWindowActionDelegate {
 
-    @Override
-    public void run(final IAction action) {
-        SetupAntDialog.open(EclipseUIUtility.getShell(),
-                CommonImages.IMG_FL_LOGO, null);
-    }
+	public void run(IAction action) {
+		DirectoryDialog dialog = new DirectoryDialog(EclipseUIUtility
+				.getShell());
+		dialog.setText(I18N
+				.msg("flashlight.eclipse.dialog.promises.saveAs.title"));
+		dialog.setMessage(I18N.msg(
+				"flashlight.eclipse.dialog.promises.saveAs.msg",
+				LibResources.ANT_TASK_VERSION));
+		final String result = dialog.open();
+		if (result != null) {
+			final File dir = new File(result, LibResources.ANT_TASK_VERSION);
+			try {
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				final File tmp = File.createTempFile("fla", "zip");
+				try {
+					FileUtility.copy(LibResources.ANT_TASK_ZIP, LibResources
+							.getAntTaskZip(), tmp);
+					FileUtility.unzipFile(tmp, dir);
+				} finally {
+					tmp.delete();
+				}
+			} catch (IOException e) {
+				SLLogger.getLogger().log(
+						Level.SEVERE,
+						I18N.err(225, LibResources.ANT_TASK_ZIP, dir
+								.getAbsolutePath()), e);
+			}
+		}
+	}
 
-    @Override
-    public void selectionChanged(final IAction action,
-            final ISelection selection) {
-        // Do nothing
-    }
+	public void init(IWorkbenchWindow window) {
+		// Nothing to do
+	}
 
-    @Override
-    public void dispose() {
-        // Do nothing
-    }
+	public void dispose() {
+		// Nothing to do
+	}
 
-    @Override
-    public void init(final IWorkbenchWindow window) {
-        // Do nothing
-    }
-
+	public void selectionChanged(IAction action, ISelection selection) {
+		// Nothing to do
+	}
 }

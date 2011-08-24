0a765c7664e798ab1353518c00c17219727feb21
Nathan Boy <nathan.boy@surelogic.com>
2011-08-24 20:39:55 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@18459 d02ae147-ee12-4a35-9452-91c89e8b2592
    Former-commit-id: 4c498a5f7343392fcb3bfd8d241b61fadfeced9a

--- src/com/surelogic/flashlight/common/prep/PrepEvent.java
+++ src/com/surelogic/flashlight/common/prep/PrepEvent.java
@@ -4,47 +4,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum PrepEvent {
-	AFTERINTRINSICLOCKACQUISITION("after-intrinsic-lock-acquisition"), AFTERINTRINSICLOCKRELEASE(
-			"after-intrinsic-lock-release"), AFTERINTRINSICLOCKWAIT(
-			"after-intrinsic-lock-wait"), AFTERUTILCONCURRENTLOCKACQUISITIONATTEMPT(
-			"after-util-concurrent-lock-acquisition-attempt"), AFTERUTILCONCURRENTLOCKRELEASEATTEMPT(
-			"after-util-concurrent-lock-release-attempt"), BEFOREINTRINSICLOCKACQUISITION(
-			"before-intrinsic-lock-acquisition"), BEFOREINTRINSICLOCKWAIT(
-			"before-intrinsic-lock-wait"), BEFOREUTILCONCURRENTLOCKACQUISITIONATTEMPT(
-			"before-util-concurrent-lock-acquisition-attempt"), CLASSDEFINITION(
-			"class-definition"), ENVIRONMENT("environment"), FIELDASSIGNMENT(
-			"field-assignment"), FIELDDEFINITION("field-definition"), FIELDREAD(
-			"field-read"), FIELDWRITE("field-write"), FINAL("final"), FLASHLIGHT(
-			"flashlight"), GARBAGECOLLECTEDOBJECT("garbage-collected-object"), INDIRECTACCESS(
-			"indirect-access"), OBJECTDEFINITION("object-definition"), READWRITELOCK(
-			"read-write-lock-definition"), SELECTEDPACKAGE("selected-package"), STATICCALLLOCATION(
-			"static-call-location"), SINGLETHREADEFIELD("single-threaded-field"), THREADDEFINITION(
-			"thread-definition"), TIME("time"), TRACENODE("trace-node");
+    AFTERINTRINSICLOCKACQUISITION("after-intrinsic-lock-acquisition"), AFTERINTRINSICLOCKRELEASE(
+            "after-intrinsic-lock-release"), AFTERINTRINSICLOCKWAIT(
+            "after-intrinsic-lock-wait"), AFTERUTILCONCURRENTLOCKACQUISITIONATTEMPT(
+            "after-util-concurrent-lock-acquisition-attempt"), AFTERUTILCONCURRENTLOCKRELEASEATTEMPT(
+            "after-util-concurrent-lock-release-attempt"), BEFOREINTRINSICLOCKACQUISITION(
+            "before-intrinsic-lock-acquisition"), BEFOREINTRINSICLOCKWAIT(
+            "before-intrinsic-lock-wait"), BEFOREUTILCONCURRENTLOCKACQUISITIONATTEMPT(
+            "before-util-concurrent-lock-acquisition-attempt"), CLASSDEFINITION(
+            "class-definition"), ENVIRONMENT("environment"), FIELDASSIGNMENT(
+            "field-assignment"), FIELDDEFINITION("field-definition"), FIELDREAD(
+            "field-read"), FIELDWRITE("field-write"), FINAL("final"), FLASHLIGHT(
+            "flashlight"), GARBAGECOLLECTEDOBJECT("garbage-collected-object"), INDIRECTACCESS(
+            "indirect-access"), OBJECTDEFINITION("object-definition"), READWRITELOCK(
+            "read-write-lock-definition"), SELECTEDPACKAGE("selected-package"), STATICCALLLOCATION(
+            "static-call-location"), SINGLETHREADEFIELD("single-threaded-field"), THREADDEFINITION(
+            "thread-definition"), TIME("time"), TRACENODE("trace-node"), CHECKPOINT(
+            "checkpoint");
 
-	static Map<String, PrepEvent> map = new HashMap<String, PrepEvent>();
+    static Map<String, PrepEvent> map = new HashMap<String, PrepEvent>();
 
-	static {
-		for (final PrepEvent p : PrepEvent.values()) {
-			map.put(p.xml, p);
-		}
-	}
+    static {
+        for (final PrepEvent p : PrepEvent.values()) {
+            map.put(p.xml, p);
+        }
+    }
 
-	public static PrepEvent getEvent(final String xml) {
-		final PrepEvent e = map.get(xml);
-		if (e == null) {
-			throw new IllegalArgumentException(xml
-					+ " is not a recognized tag.");
-		}
-		return e;
-	}
+    public static PrepEvent getEvent(final String xml) {
+        final PrepEvent e = map.get(xml);
+        if (e == null) {
+            throw new IllegalArgumentException(xml
+                    + " is not a recognized tag.");
+        }
+        return e;
+    }
 
-	PrepEvent(final String xmlName) {
-		this.xml = xmlName;
-	}
+    PrepEvent(final String xmlName) {
+        this.xml = xmlName;
+    }
 
-	private String xml;
+    private String xml;
 
-	public String getXmlName() {
-		return xml;
-	}
+    public String getXmlName() {
+        return xml;
+    }
 }

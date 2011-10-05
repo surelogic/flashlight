d3b49e8925ec0b62637e364baf49478bf402e4f9
Nathan Boy <nathan.boy@surelogic.com>
2011-10-05 21:00:49 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@18776 d02ae147-ee12-4a35-9452-91c89e8b2592
    Former-commit-id: 5c99e29f983feb481ef3fe2e0841f7e7621c24d7

--- src/com/surelogic/flashlight/common/files/InstrumentationFileHandles.java
+++ src/com/surelogic/flashlight/common/files/InstrumentationFileHandles.java
@@ -14,98 +14,98 @@ import com.surelogic.common.logging.SLLogger;
  * @see RunDirectory
  */
 public final class InstrumentationFileHandles {
-	/** The standard name for the instrumentation field database file. */
-	public static final String FIELDS_FILE_NAME = "fields.txt";
-	/** The standard name for the instrumentation sites field database file. */
-	public static final String SITES_FILE_NAME = "sites.txt";
-	/** The standard name for the instrumentation log file. */
-	public static final String INSTRUMENTATION_LOG_FILE_NAME = "instrumentation.log";
+    /** The standard name for the instrumentation field database file. */
+    public static final String FIELDS_FILE_NAME = "fields.txt";
+    /** The standard name for the instrumentation sites field database file. */
+    public static final String SITES_FILE_NAME = "sites.txt";
+    /** The standard name for the instrumentation log file. */
+    public static final String INSTRUMENTATION_LOG_FILE_NAME = "instrumentation.log";
 
-	private final File fieldsFileHandle;
-	private final File sitesFileHandle;
-	private final File logFileHandle;
+    private final File fieldsFileHandle;
+    private final File sitesFileHandle;
+    private final File logFileHandle;
 
-	/* Private: use the factory method */
-	private InstrumentationFileHandles(final File fieldsFile,
-			final File sitesFile, final File logFile) {
-		fieldsFileHandle = fieldsFile;
-		sitesFileHandle = sitesFile;
-		logFileHandle = logFile;
-	}
+    /* Private: use the factory method */
+    private InstrumentationFileHandles(final File fieldsFile,
+            final File sitesFile, final File logFile) {
+        fieldsFileHandle = fieldsFile;
+        sitesFileHandle = sitesFile;
+        logFileHandle = logFile;
+    }
 
-	/**
-	 * Checks to make sure the appropriate file objects exist in this run
-	 * director.
-	 * 
-	 * @param runDir
-	 *            the run directory to scan
-	 * @return <code>true</code> when all the instrumentation files are present,
-	 *         false otherwise
-	 */
-	static boolean hasValidHandles(final File runDir) {
-		final File fieldsFile = new File(runDir, FIELDS_FILE_NAME);
-		final File sitesFile = new File(runDir, SITES_FILE_NAME);
-		final File sitesGzipFile = new File(runDir, SITES_FILE_NAME
-				+ FileUtility.GZIP_SUFFIX);
-		final File logFile = new File(runDir, INSTRUMENTATION_LOG_FILE_NAME);
-		return fieldsFile.exists()
-				&& (sitesFile.exists() || sitesGzipFile.exists())
-				&& logFile.exists();
-	}
+    /**
+     * Checks to make sure the appropriate file objects exist in this run
+     * director.
+     * 
+     * @param runDir
+     *            the run directory to scan
+     * @return <code>true</code> when all the instrumentation files are present,
+     *         false otherwise
+     */
+    static boolean hasValidHandles(final File runDir) {
+        final File fieldsFile = new File(runDir, FIELDS_FILE_NAME);
+        final File sitesFile = new File(runDir, SITES_FILE_NAME);
+        final File sitesGzipFile = new File(runDir, SITES_FILE_NAME
+                + FileUtility.GZIP_SUFFIX);
+        final File logFile = new File(runDir, INSTRUMENTATION_LOG_FILE_NAME);
+        return fieldsFile.exists()
+                && (sitesFile.exists() || sitesGzipFile.exists())
+                && logFile.exists();
+    }
 
-	/**
-	 * <i>Implementation Note:</i> This constructor scans the Flashlight data
-	 * directory.
-	 * 
-	 * @return The handles object. Return {@code null} if one of the files
-	 *         cannot be found.
-	 */
-	/* Package private: Can only be created by a RunDirectory */
-	static InstrumentationFileHandles getFor(final File runDir) {
-		final File fieldsFile = new File(runDir, FIELDS_FILE_NAME);
-		final File sitesFile = new File(runDir, SITES_FILE_NAME);
-		final File sitesGzipFile = new File(runDir, SITES_FILE_NAME
-				+ FileUtility.GZIP_SUFFIX);
-		final File logFile = new File(runDir, INSTRUMENTATION_LOG_FILE_NAME);
+    /**
+     * <i>Implementation Note:</i> This constructor scans the Flashlight data
+     * directory.
+     * 
+     * @return The handles object. Return {@code null} if one of the files
+     *         cannot be found.
+     */
+    /* Package private: Can only be created by a RunDirectory */
+    static InstrumentationFileHandles getFor(final File runDir) {
+        final File fieldsFile = new File(runDir, FIELDS_FILE_NAME);
+        final File sitesFile = new File(runDir, SITES_FILE_NAME);
+        final File sitesGzipFile = new File(runDir, SITES_FILE_NAME
+                + FileUtility.GZIP_SUFFIX);
+        final File logFile = new File(runDir, INSTRUMENTATION_LOG_FILE_NAME);
 
-		if (runDir == null || !runDir.exists()) {
-			return null;
-		}
-		boolean failed = false;
-		if (!fieldsFile.exists()) {
-			failed = true;
-			SLLogger.getLogger().log(Level.WARNING,
-					I18N.err(148, runDir.getAbsolutePath(), FIELDS_FILE_NAME));
-		}
-		if (!sitesFile.exists() && !sitesGzipFile.exists()) {
-			failed = true;
-			SLLogger.getLogger().log(Level.WARNING,
-					I18N.err(149, runDir.getAbsolutePath(), SITES_FILE_NAME));
-		}
-		if (!logFile.exists()) {
-			failed = true;
-			SLLogger.getLogger().log(
-					Level.WARNING,
-					I18N.err(150, runDir.getAbsolutePath(),
-							INSTRUMENTATION_LOG_FILE_NAME));
-		}
-		if (failed) {
-			return null;
-		} else {
-			return new InstrumentationFileHandles(fieldsFile, sitesFile,
-					logFile);
-		}
-	}
+        if (runDir == null || !runDir.exists()) {
+            return null;
+        }
+        boolean failed = false;
+        if (!fieldsFile.exists()) {
+            failed = true;
+            SLLogger.getLogger().log(Level.WARNING,
+                    I18N.err(148, runDir.getAbsolutePath(), FIELDS_FILE_NAME));
+        }
+        if (!sitesFile.exists() && !sitesGzipFile.exists()) {
+            failed = true;
+            SLLogger.getLogger().log(Level.WARNING,
+                    I18N.err(149, runDir.getAbsolutePath(), SITES_FILE_NAME));
+        }
+        if (!logFile.exists()) {
+            failed = true;
+            SLLogger.getLogger().log(
+                    Level.WARNING,
+                    I18N.err(150, runDir.getAbsolutePath(),
+                            INSTRUMENTATION_LOG_FILE_NAME));
+        }
+        if (failed) {
+            return null;
+        } else {
+            return new InstrumentationFileHandles(fieldsFile, sitesFile,
+                    logFile);
+        }
+    }
 
-	public File getFieldsFile() {
-		return fieldsFileHandle;
-	}
+    public File getFieldsFile() {
+        return fieldsFileHandle;
+    }
 
-	public File getSitesFile() {
-		return sitesFileHandle;
-	}
+    public File getSitesFile() {
+        return sitesFileHandle;
+    }
 
-	public File getInstrumentationLogFile() {
-		return logFileHandle;
-	}
+    public File getInstrumentationLogFile() {
+        return logFileHandle;
+    }
 }

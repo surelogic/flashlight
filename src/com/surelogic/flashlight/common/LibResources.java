846e6df05b0efaebc8bf953469acfc45c776f9eb
Nathan Boy <nathan.boy@surelogic.com>
2011-11-02 20:48:46 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/flashlight-common@18939 d02ae147-ee12-4a35-9452-91c89e8b2592
    Former-commit-id: 9eb48875aad9b4fe1debbe580a5e54b7847eba1f

--- src/com/surelogic/flashlight/common/LibResources.java
+++ src/com/surelogic/flashlight/common/LibResources.java
@@ -5,23 +5,23 @@ import java.io.InputStream;
 import java.net.URL;
 
 public final class LibResources {
-	/**
-	 * The name of the current version Flashlight Ant tasks. This is used for
-	 * the directory created by the tool.
-	 */
-	public static final String ANT_TASK_VERSION = "flashlight-ant-4.0.0";
+    /**
+     * The name of the current version Flashlight Ant tasks. This is used for
+     * the directory created by the tool.
+     */
+    public static final String ANT_TASK_VERSION = "flashlight-ant-4.0.2";
 
-	/**
-	 * The name of the archive that contains the Flashlight Ant tasks.
-	 */
-	public static final String ANT_TASK_ZIP = "flashlight-ant.zip";
+    /**
+     * The name of the archive that contains the Flashlight Ant tasks.
+     */
+    public static final String ANT_TASK_ZIP = "flashlight-ant.zip";
 
-	public static final String PATH = "/lib/";
-	public static final String ANT_TASK_ZIP_PATHNAME = PATH + ANT_TASK_ZIP;
+    public static final String PATH = "/lib/";
+    public static final String ANT_TASK_ZIP_PATHNAME = PATH + ANT_TASK_ZIP;
 
-	public static InputStream getAntTaskZip() throws IOException {
-		final URL url = LibResources.class.getResource(ANT_TASK_ZIP_PATHNAME);
-		final InputStream is = url.openStream();
-		return is;
-	}
+    public static InputStream getAntTaskZip() throws IOException {
+        final URL url = LibResources.class.getResource(ANT_TASK_ZIP_PATHNAME);
+        final InputStream is = url.openStream();
+        return is;
+    }
 }

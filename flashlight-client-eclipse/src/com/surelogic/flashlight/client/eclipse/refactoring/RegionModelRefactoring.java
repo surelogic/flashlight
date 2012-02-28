165578c8701ff9e03eaaec216b44d7d1f7687ae2
Nathan Boy <nathan.boy@gmail.com>
2012-02-28 11:40:12 -0500

    Fixed some compiler warnings in the Flashlight package
    Former-commit-id: 30c3404d8e036250658eae8298ac3f60f8e0b818

--- flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/refactoring/RegionModelRefactoring.java
+++ flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/refactoring/RegionModelRefactoring.java
@@ -23,214 +23,160 @@ import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.text.edits.TextEdit;
 import org.eclipse.text.edits.TextEditGroup;
 
-import com.surelogic.common.ui.refactor.PromisesAnnotationRewriter;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.jdbc.DBConnection;
 import com.surelogic.common.logging.SLLogger;
+import com.surelogic.common.ui.refactor.PromisesAnnotationRewriter;
 import com.surelogic.flashlight.common.model.RunDescription;
 import com.surelogic.flashlight.recommend.RecommendRegions;
 import com.surelogic.flashlight.recommend.RecommendedRegion;
 
 public class RegionModelRefactoring extends Refactoring {
-	private static final Logger LOG = SLLogger
-			.getLoggerFor(RegionModelRefactoring.class);
-	private final RegionRefactoringInfo info;
-	private Map<String, Map<String, RecommendedRegion>> targetRegions;
-	private Map<String, Map<String, RecommendedRegion>> targetClassRegions;
-	private Map<String, Map<String, List<RecommendedRegion>>> targetFieldRegions;
+    private static final Logger LOG = SLLogger
+            .getLoggerFor(RegionModelRefactoring.class);
+    private final RegionRefactoringInfo info;
+    private Map<String, Map<String, RecommendedRegion>> targetRegions;
+    private Map<String, Map<String, RecommendedRegion>> targetClassRegions;
+    private Map<String, Map<String, List<RecommendedRegion>>> targetFieldRegions;
 
-	public RegionModelRefactoring(final RegionRefactoringInfo info) {
-		this.info = info;
-	}
+    public RegionModelRefactoring(final RegionRefactoringInfo info) {
+        this.info = info;
+    }
 
-	@Override
-	public RefactoringStatus checkFinalConditions(final IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
-		targetRegions = new HashMap<String, Map<String, RecommendedRegion>>();
-		targetClassRegions = new HashMap<String, Map<String, RecommendedRegion>>();
-		targetFieldRegions = new HashMap<String, Map<String, List<RecommendedRegion>>>();
-		for (final RunDescription desc : info.getSelectedRuns()) {
-			final DBConnection conn = desc.getDB();
-			try {
-				conn.bootAndCheckSchema();
-				mergeRegions(targetRegions, conn.withReadOnly(RecommendRegions
-						.lockIsThisRegions()));
-				mergeRegions(targetClassRegions, conn
-						.withReadOnly(RecommendRegions.lockIsClassRegions()));
-				for (final RecommendedRegion r : conn
-						.withReadOnly(RecommendRegions
-								.recommendedFieldLockRegions())) {
-					Map<String, List<RecommendedRegion>> map = targetFieldRegions
-							.get(r.getPackage());
-					if (map == null) {
-						map = new HashMap<String, List<RecommendedRegion>>();
-						targetFieldRegions.put(r.getPackage(), map);
-					}
-					List<RecommendedRegion> list = map.get(r.getClazz());
-					if (list == null) {
-						list = new ArrayList<RecommendedRegion>();
-						map.put(r.getClazz(), list);
-					}
-					list.add(r);
-				}
-			} catch (final Exception e) {
-				LOG.log(Level.WARNING, e.getMessage(), e);
-				return RefactoringStatus.createErrorStatus(e.getMessage());
-			}
-		}
-		return RefactoringStatus.create(Status.OK_STATUS);
-	}
+    @Override
+    public RefactoringStatus checkFinalConditions(final IProgressMonitor pm)
+            throws CoreException, OperationCanceledException {
+        targetRegions = new HashMap<String, Map<String, RecommendedRegion>>();
+        targetClassRegions = new HashMap<String, Map<String, RecommendedRegion>>();
+        targetFieldRegions = new HashMap<String, Map<String, List<RecommendedRegion>>>();
+        for (final RunDescription desc : info.getSelectedRuns()) {
+            final DBConnection conn = desc.getDB();
+            try {
+                conn.bootAndCheckSchema();
+                mergeRegions(targetRegions,
+                        conn.withReadOnly(RecommendRegions.lockIsThisRegions()));
+                mergeRegions(
+                        targetClassRegions,
+                        conn.withReadOnly(RecommendRegions.lockIsClassRegions()));
+                for (final RecommendedRegion r : conn
+                        .withReadOnly(RecommendRegions
+                                .recommendedFieldLockRegions())) {
+                    Map<String, List<RecommendedRegion>> map = targetFieldRegions
+                            .get(r.getPackage());
+                    if (map == null) {
+                        map = new HashMap<String, List<RecommendedRegion>>();
+                        targetFieldRegions.put(r.getPackage(), map);
+                    }
+                    List<RecommendedRegion> list = map.get(r.getClazz());
+                    if (list == null) {
+                        list = new ArrayList<RecommendedRegion>();
+                        map.put(r.getClazz(), list);
+                    }
+                    list.add(r);
+                }
+            } catch (final Exception e) {
+                LOG.log(Level.WARNING, e.getMessage(), e);
+                return RefactoringStatus.createErrorStatus(e.getMessage());
+            }
+        }
+        return RefactoringStatus.create(Status.OK_STATUS);
+    }
 
-	/**
-	 * Utility method used to merge the results of two different runs into a
-	 * unified set of recommended regions
-	 * 
-	 * @param regions
-	 * @param two
-	 * @return
-	 */
-	private static void mergeRegions(
-			final Map<String, Map<String, RecommendedRegion>> regions,
-			final List<RecommendedRegion> list) {
-		for (final RecommendedRegion r : list) {
-			Map<String, RecommendedRegion> packageRegion = regions.get(r
-					.getPackage());
-			if (packageRegion == null) {
-				packageRegion = new HashMap<String, RecommendedRegion>();
-				regions.put(r.getPackage(), packageRegion);
-			}
-			final RecommendedRegion clazzRegion = packageRegion.get(r
-					.getClazz());
-			if (clazzRegion == null) {
-				packageRegion.put(r.getClazz(), r);
-			} else {
-				clazzRegion.addFields(r.getFields());
-			}
-		}
-	}
+    /**
+     * Utility method used to merge the results of two different runs into a
+     * unified set of recommended regions
+     * 
+     * @param regions
+     * @param two
+     * @return
+     */
+    private static void mergeRegions(
+            final Map<String, Map<String, RecommendedRegion>> regions,
+            final List<RecommendedRegion> list) {
+        for (final RecommendedRegion r : list) {
+            Map<String, RecommendedRegion> packageRegion = regions.get(r
+                    .getPackage());
+            if (packageRegion == null) {
+                packageRegion = new HashMap<String, RecommendedRegion>();
+                regions.put(r.getPackage(), packageRegion);
+            }
+            final RecommendedRegion clazzRegion = packageRegion.get(r
+                    .getClazz());
+            if (clazzRegion == null) {
+                packageRegion.put(r.getClazz(), r);
+            } else {
+                clazzRegion.addFields(r.getFields());
+            }
+        }
+    }
 
-	@Override
-	public RefactoringStatus checkInitialConditions(final IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
-		// TODO Quick checks
-		return RefactoringStatus.create(Status.OK_STATUS);
-	}
+    @Override
+    public RefactoringStatus checkInitialConditions(final IProgressMonitor pm)
+            throws CoreException, OperationCanceledException {
+        // TODO Quick checks
+        return RefactoringStatus.create(Status.OK_STATUS);
+    }
 
-	@Override
-	public Change createChange(final IProgressMonitor pm) throws CoreException,
-			OperationCanceledException {
-		final CompositeChange root = new CompositeChange(String.format(
-				"Changes to %s", info.getSelectedProject().getProject()
-						.getName()));
-		final TextEditGroup g = new TextEditGroup(
-				"Adding @RegionLock annotations");
-		for (final IPackageFragment fragment : info.getSelectedProject()
-				.getPackageFragments()) {
-			final String packageName = fragment.getElementName();
-			final Map<String, RecommendedRegion> targetMap = targetRegions
-					.get(packageName);
-			final Map<String, RecommendedRegion> targetClassMap = targetClassRegions
-					.get(packageName);
-			final Map<String, List<RecommendedRegion>> targetFieldMap = targetFieldRegions
-					.get(packageName);
-			final PromisesAnnotationRewriter rewrite = new PromisesAnnotationRewriter();
+    @Override
+    public Change createChange(final IProgressMonitor pm) throws CoreException,
+            OperationCanceledException {
+        final CompositeChange root = new CompositeChange(String.format(
+                "Changes to %s", info.getSelectedProject().getProject()
+                        .getName()));
+        final TextEditGroup g = new TextEditGroup(
+                "Adding @RegionLock annotations");
+        for (final IPackageFragment fragment : info.getSelectedProject()
+                .getPackageFragments()) {
+            final String packageName = fragment.getElementName();
+            final Map<String, RecommendedRegion> targetMap = targetRegions
+                    .get(packageName);
+            final Map<String, RecommendedRegion> targetClassMap = targetClassRegions
+                    .get(packageName);
+            final Map<String, List<RecommendedRegion>> targetFieldMap = targetFieldRegions
+                    .get(packageName);
+            final PromisesAnnotationRewriter rewrite = new PromisesAnnotationRewriter();
 
-			for (final ICompilationUnit cu : fragment.getCompilationUnits()) {
-				final RegionModel model = new RegionModel();
-				for (final IType type : cu.getTypes()) {
-					final String typeName = type.getElementName();
-					model.mergeRegion(targetMap == null ? null : targetMap
-							.get(typeName));
-					model.mergeRegion(targetClassMap == null ? null
-							: targetClassMap.get(typeName));
-					if (targetFieldMap != null) {
-						final List<RecommendedRegion> fieldRegions = targetFieldMap
-								.get(typeName);
-						if (fieldRegions != null) {
-							for (final RecommendedRegion r : fieldRegions) {
-								model.mergeRegion(r);
-							}
-						}
-					}
-				}
-				if (!model.isEmpty()) {
-					final Map<String, TypeNode> typeMap = TypeReconciler
-							.createTypeMap(cu);
-					final NamingScheme ns = NamingScheme.DEFAULT;
-					rewrite.setCompilationUnit(cu);
-					rewrite.writeAnnotations(model.generateAnnotations(typeMap,
-							ns), g);
-					final TextEdit edit = rewrite.getTextEdit();
-					final IFile file = (IFile) cu.getResource();
-					final TextFileChange change = new TextFileChange(file
-							.getName(), file);
-					change.setEdit(edit);
-					root.add(change);
-				}
-			}
-		}
+            for (final ICompilationUnit cu : fragment.getCompilationUnits()) {
+                final RegionModel model = new RegionModel();
+                for (final IType type : cu.getTypes()) {
+                    final String typeName = type.getElementName();
+                    model.mergeRegion(targetMap == null ? null : targetMap
+                            .get(typeName));
+                    model.mergeRegion(targetClassMap == null ? null
+                            : targetClassMap.get(typeName));
+                    if (targetFieldMap != null) {
+                        final List<RecommendedRegion> fieldRegions = targetFieldMap
+                                .get(typeName);
+                        if (fieldRegions != null) {
+                            for (final RecommendedRegion r : fieldRegions) {
+                                model.mergeRegion(r);
+                            }
+                        }
+                    }
+                }
+                if (!model.isEmpty()) {
+                    final Map<String, TypeNode> typeMap = TypeReconciler
+                            .createTypeMap(cu);
+                    final NamingScheme ns = NamingScheme.DEFAULT;
+                    rewrite.setCompilationUnit(cu);
+                    rewrite.writeAnnotations(
+                            model.generateAnnotations(typeMap, ns), g);
+                    final TextEdit edit = rewrite.getTextEdit();
+                    final IFile file = (IFile) cu.getResource();
+                    final TextFileChange change = new TextFileChange(
+                            file.getName(), file);
+                    change.setEdit(edit);
+                    root.add(change);
+                }
+            }
+        }
 
-		return root;
-	}
+        return root;
+    }
 
-	// @Override
-	// public Change createChange(final IProgressMonitor pm) throws
-	// CoreException,
-	// OperationCanceledException {
-	// final CompositeChange root = new CompositeChange(String.format(
-	// "Changes to %s", info.getSelectedProject().getProject()
-	// .getName()));
-	// final RegionRewriter rewriter = new RegionRewriter(info
-	// .getSelectedProject());
-	// final TextEditGroup g = new TextEditGroup(
-	// "Adding @RegionLock annotations");
-	// for (final IPackageFragment fragment : info.getSelectedProject()
-	// .getPackageFragments()) {
-	// final String packageName = fragment.getElementName();
-	// final Map<String, RecommendedRegion> targetMap = targetRegions
-	// .get(packageName);
-	// final Map<String, RecommendedRegion> targetClassMap = targetClassRegions
-	// .get(packageName);
-	// final Map<String, List<RecommendedRegion>> targetFieldMap =
-	// targetFieldRegions
-	// .get(packageName);
-	// for (final ICompilationUnit cu : fragment.getCompilationUnits()) {
-	// rewriter.setCompilationUnit(cu);
-	// final RegionModel model = new RegionModel();
-	// for (final IType type : cu.getTypes()) {
-	// final String typeName = type.getElementName();
-	// model.mergeRegion(targetMap == null ? null : targetMap
-	// .get(typeName));
-	// model.mergeRegion(targetClassMap == null ? null
-	// : targetClassMap.get(typeName));
-	// if (targetFieldMap != null) {
-	// final List<RecommendedRegion> fieldRegions = targetFieldMap
-	// .get(typeName);
-	// if (fieldRegions != null) {
-	// for (final RecommendedRegion r : fieldRegions) {
-	// model.mergeRegion(r);
-	// }
-	// }
-	// }
-	// }
-	// if (!model.isEmpty()) {
-	//
-	// rewriter.rewriteRegionModel(g, model);
-	// final TextEdit edit = rewriter.getTextEdit();
-	// final IFile file = (IFile) cu.getResource();
-	// final TextFileChange change = new TextFileChange(file
-	// .getName(), file);
-	// change.setEdit(edit);
-	// root.add(change);
-	// }
-	// }
-	// }
-	// return root;
-	// }
-
-	@Override
-	public String getName() {
-		return I18N.msg("flashlight.recommend.refactor.regionIsThis");
-	}
+    @Override
+    public String getName() {
+        return I18N.msg("flashlight.recommend.refactor.regionIsThis");
+    }
 
 }
--- flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/refactoring/RegionModelRefactoringAction.java
+++ flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/refactoring/RegionModelRefactoringAction.java
@@ -21,59 +21,64 @@ import com.surelogic.flashlight.common.model.RunDescription;
 import com.surelogic.flashlight.common.model.RunManager;
 
 public class RegionModelRefactoringAction implements IObjectActionDelegate,
-		IWorkbenchWindowActionDelegate {
+        IWorkbenchWindowActionDelegate {
 
-	private IJavaProject f_javaProject = null;
+    private IJavaProject f_javaProject = null;
 
-	public void setActivePart(final IAction action,
-			final IWorkbenchPart targetPart) {
-		// Do nothing
-	}
+    @Override
+    public void setActivePart(final IAction action,
+            final IWorkbenchPart targetPart) {
+        // Do nothing
+    }
 
-	public void run(final IAction action) {
-		final List<RunDescription> preppedRuns = new ArrayList<RunDescription>(
-				RunManager.getInstance().getPreparedRunDescriptions());
+    @Override
+    public void run(final IAction action) {
+        final List<RunDescription> preppedRuns = new ArrayList<RunDescription>(
+                RunManager.getInstance().getPreparedRunDescriptions());
 
-		final RegionRefactoringInfo info = new RegionRefactoringInfo(
-				JDTUtility.getJavaProjects(), preppedRuns);
-		info.setSelectedProject(f_javaProject);
-		final RegionModelRefactoring refactoring = new RegionModelRefactoring(
-				info);
-		final RegionRefactoringWizard wizard = new RegionRefactoringWizard(
-				refactoring, info);
-		final RefactoringWizardOpenOperation op = new RefactoringWizardOpenOperation(
-				wizard);
-		try {
-			if (op.run(EclipseUIUtility.getShell(),
-					I18N.msg("flashlight.recommend.refactor.regionIsThis")) == IDialogConstants.OK_ID) {
-				try {
-					// TODO add jar and prompt to analyze
-				} catch (final NoClassDefFoundError e) {
-					// This is expected if jsure is not installed
-				}
-			}
-		} catch (final InterruptedException e) {
-			// Operation was cancelled. Whatever floats their boat.
-		}
-	}
+        final RegionRefactoringInfo info = new RegionRefactoringInfo(
+                JDTUtility.getJavaProjects(), preppedRuns);
+        info.setSelectedProject(f_javaProject);
+        final RegionModelRefactoring refactoring = new RegionModelRefactoring(
+                info);
+        final RegionRefactoringWizard wizard = new RegionRefactoringWizard(
+                refactoring, info);
+        final RefactoringWizardOpenOperation op = new RefactoringWizardOpenOperation(
+                wizard);
+        try {
+            if (op.run(EclipseUIUtility.getShell(),
+                    I18N.msg("flashlight.recommend.refactor.regionIsThis")) == IDialogConstants.OK_ID) {
+                try {
+                    // TODO add jar and prompt to analyze
+                } catch (final NoClassDefFoundError e) {
+                    // This is expected if jsure is not installed
+                }
+            }
+        } catch (final InterruptedException e) {
+            // Operation was cancelled. Whatever floats their boat.
+        }
+    }
 
-	public void selectionChanged(final IAction action,
-			final ISelection selection) {
-		if (selection instanceof IStructuredSelection) {
-			final Object o = ((IStructuredSelection) selection)
-					.getFirstElement();
-			if (o instanceof IJavaProject) {
-				f_javaProject = (IJavaProject) o;
-				// FIXME action.setEnabled(noCompilationErrors(f_javaProject));
-			}
-		}
-	}
+    @Override
+    public void selectionChanged(final IAction action,
+            final ISelection selection) {
+        if (selection instanceof IStructuredSelection) {
+            final Object o = ((IStructuredSelection) selection)
+                    .getFirstElement();
+            if (o instanceof IJavaProject) {
+                f_javaProject = (IJavaProject) o;
+                // FIXME action.setEnabled(noCompilationErrors(f_javaProject));
+            }
+        }
+    }
 
-	public void dispose() {
-		// Do nothing
-	}
+    @Override
+    public void dispose() {
+        // Do nothing
+    }
 
-	public void init(final IWorkbenchWindow window) {
-		// Do nothing
-	}
+    @Override
+    public void init(final IWorkbenchWindow window) {
+        // Do nothing
+    }
 }

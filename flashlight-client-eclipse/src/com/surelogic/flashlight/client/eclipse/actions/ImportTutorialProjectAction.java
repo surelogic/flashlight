057e26d1be087aa753bc1785a79d2ec121f6c8d3
Tim Halloran <tim.halloran@surelogic.com>
2012-05-24 16:39:33 -0400

    Fixed tutorial names
    Former-commit-id: 263c425cb402e23d9dd02fb83b13685d0dfdb341

--- flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/actions/ImportTutorialProjectAction.java
+++ flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/actions/ImportTutorialProjectAction.java
@@ -10,34 +10,34 @@ import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.common.ui.dialogs.InstallTutorialProjectsDialog;
 
 public class ImportTutorialProjectAction implements
-        IWorkbenchWindowActionDelegate {
+		IWorkbenchWindowActionDelegate {
 
-    @Override
-    public void dispose() {
-        // Do nothing
-    }
+	@Override
+	public void dispose() {
+		// Do nothing
+	}
 
-    @Override
-    public void init(final IWorkbenchWindow window) {
-        // Do nothing
-    }
+	@Override
+	public void init(final IWorkbenchWindow window) {
+		// Do nothing
+	}
 
-    @Override
-    public void run(final IAction action) {
-        ClassLoader l = Thread.currentThread().getContextClassLoader();
-        InstallTutorialProjectsDialog.open(EclipseUIUtility.getShell(),
-                CommonImages.IMG_FL_LOGO,
-                "/com.surelogic.flashlight.client.help/ch01s03.html",
-                l.getResource("/lib/DiningPhilosophers.zip"),
-                l.getResource("/lib/jEdit-4.1.zip"),
-                l.getResource("/lib/PlanetBaron.zip"),
-                l.getResource("/lib/Zookeeper.zip"));
-    }
+	@Override
+	public void run(final IAction action) {
+		ClassLoader l = Thread.currentThread().getContextClassLoader();
+		InstallTutorialProjectsDialog
+				.open(EclipseUIUtility.getShell(),
+						CommonImages.IMG_FL_LOGO,
+						"/com.surelogic.flashlight.client.help/ch01s03.html",
+						l.getResource("/lib/FlashlightTutorial_PlanetBaron.zip"),
+						l.getResource("/lib/FlashlightTutorial_DiningPhilosophers.zip"),
+						l.getResource("/lib/FlashlightTutorial_jEdit-4.1.zip"),
+						l.getResource("/lib/FlashlightTutorial_Zookeeper.zip"));
+	}
 
-    @Override
-    public void selectionChanged(final IAction action,
-            final ISelection selection) {
-        // Do nothing
-    }
-
+	@Override
+	public void selectionChanged(final IAction action,
+			final ISelection selection) {
+		// Do nothing
+	}
 }

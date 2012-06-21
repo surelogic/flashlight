bacb05396367ad079c8f426639203589313a193c
Nathan Boy <nathan.boy@gmail.com>
2012-06-21 09:57:06 -0400

    Checkin of additional getId method to SiteIdFactory.
    Former-commit-id: 0c5adea8adb96005640258b74e8fcb975c48ba70

--- flashlight-instrumentation-5.0/src-asm/com/surelogic/_flashlight/rewriter/SiteIdFactory.java
+++ flashlight-instrumentation-5.0/src-asm/com/surelogic/_flashlight/rewriter/SiteIdFactory.java
@@ -6,79 +6,91 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public final class SiteIdFactory {
-  private final PrintWriter pw;
-  private long nextId = 0L;
-  private final SortedMap<Integer, Long> linesToSites = new TreeMap<Integer, Long>();
-  private final SortedMap<Long, Integer> sitesToLines = new TreeMap<Long, Integer>();
-  private String sourceFileName;
-  private String className;
-  private String callingMethodName;
-  
-  public SiteIdFactory(final PrintWriter pw) {
-    this.pw = pw;
-  }
-  
-  public void setMethodLocation(final String sourceFileName,
-      final String className, final String callingMethodName) {
-    this.sourceFileName = sourceFileName;
-    this.className = className;
-    this.callingMethodName = callingMethodName;
-    linesToSites.clear();
-    sitesToLines.clear();
-  }
-  
-  public long getSiteId(final int lineNumber) {
-    final Long existingSiteId = linesToSites.get(lineNumber);
-    if (existingSiteId == null) {
-      /* We haven't seen this location before */
-      final long id = nextId++;
-      linesToSites.put(lineNumber, id);
-      sitesToLines.put(id, lineNumber);
-      
-//      /* Log the site to the database */
-//      recordSiteToDatabase(id, lineNumber);
-      return id;
-    } else {
-      /* We have seen this location before */
-      return existingSiteId.longValue();
-    }
-  }
+    private final PrintWriter pw;
+    private long nextId = 0L;
+    private final SortedMap<Integer, Long> linesToSites = new TreeMap<Integer, Long>();
+    private final SortedMap<Long, Integer> sitesToLines = new TreeMap<Long, Integer>();
+    private String sourceFileName;
+    private String className;
+    private String callingMethodName;
 
-  private void recordSiteToDatabase(final long siteId, final int line) {
-    pw.print(siteId);
-    pw.print(' ');
-    pw.print(sourceFileName);
-    pw.print(' ');
-    pw.print(className);
-    pw.print(' ');
-    pw.print(callingMethodName);
-    pw.print(' ');
-    pw.println(line);
-  }
-  
-  public void closeMethod() {
-    /* If possible, reassociate the site associated with line number -1 with
-     * the first non-negative line number holding a site.
-     */ 
-    Long emptySite = null;
-    Integer firstRealLineNumber = null;
-    for (final Map.Entry<Integer, Long> entry : linesToSites.entrySet()) {
-      final Integer lineNum = entry.getKey();
-      final int line = lineNum.intValue();
-      if (line == -1) {
-        emptySite = entry.getValue();
-      } else if (line >= 0) {
-        firstRealLineNumber = lineNum;
-        break;
-      }
+    private String calledMethodName;
+    private String calledMethodClass;
+
+    public SiteIdFactory(final PrintWriter pw) {
+        this.pw = pw;
     }
-    if (emptySite != null && firstRealLineNumber != null) {
-      sitesToLines.put(emptySite, firstRealLineNumber);
+
+    public void setMethodLocation(final String sourceFileName,
+            final String className, final String callingMethodName) {
+        this.sourceFileName = sourceFileName;
+        this.className = className;
+        this.callingMethodName = callingMethodName;
+        linesToSites.clear();
+        sitesToLines.clear();
     }
-    
-    /* Output all the sites to the database */
-    for (final Map.Entry<Long, Integer> entry : sitesToLines.entrySet()) {
-      recordSiteToDatabase(entry.getKey(), entry.getValue());
+
+    public long getSiteId(final int lineNumber) {
+        final Long existingSiteId = linesToSites.get(lineNumber);
+        if (existingSiteId == null) {
+            /* We haven't seen this location before */
+            final long id = nextId++;
+            linesToSites.put(lineNumber, id);
+            sitesToLines.put(id, lineNumber);
+
+            // /* Log the site to the database */
+            // recordSiteToDatabase(id, lineNumber);
+            return id;
+        } else {
+            /* We have seen this location before */
+            return existingSiteId.longValue();
+        }
     }
-  }
+
+    public long getSiteId(final int lineNumber, final String calledMethodName,
+            final String calledMethodClass) {
+        long id = getSiteId(lineNumber);
+        this.calledMethodName = calledMethodName;
+        this.calledMethodClass = calledMethodClass;
+        return id;
+    }
+
+    private void recordSiteToDatabase(final long siteId, final int line) {
+        pw.print(siteId);
+        pw.print(' ');
+        pw.print(sourceFileName);
+        pw.print(' ');
+        pw.print(className);
+        pw.print(' ');
+        pw.print(callingMethodName);
+        pw.print(' ');
+        pw.println(line);
+    }
+
+    public void closeMethod() {
+        /*
+         * If possible, reassociate the site associated with line number -1 with
+         * the first non-negative line number holding a site.
+         */
+        Long emptySite = null;
+        Integer firstRealLineNumber = null;
+        for (final Map.Entry<Integer, Long> entry : linesToSites.entrySet()) {
+            final Integer lineNum = entry.getKey();
+            final int line = lineNum.intValue();
+            if (line == -1) {
+                emptySite = entry.getValue();
+            } else if (line >= 0) {
+                firstRealLineNumber = lineNum;
+                break;
+            }
+        }
+        if (emptySite != null && firstRealLineNumber != null) {
+            sitesToLines.put(emptySite, firstRealLineNumber);
+        }
+
+        /* Output all the sites to the database */
+        for (final Map.Entry<Long, Integer> entry : sitesToLines.entrySet()) {
+            recordSiteToDatabase(entry.getKey(), entry.getValue());
+        }
+    }
 }

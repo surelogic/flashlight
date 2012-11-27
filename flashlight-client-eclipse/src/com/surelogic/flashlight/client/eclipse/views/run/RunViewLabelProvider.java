e0d45d14bf80d896d1d3485e02449b6ca02af958
Tim Halloran <tim.halloran@surelogic.com>
2012-11-27 16:46:50 -0500

    Fixed image reference
    Former-commit-id: be29640a49619595b27b1b2a819b37cdc5f721ef

--- flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/views/run/RunViewLabelProvider.java
+++ flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/views/run/RunViewLabelProvider.java
@@ -10,41 +10,49 @@ import com.surelogic.flashlight.common.model.RunViewModel;
 
 public final class RunViewLabelProvider implements ITableLabelProvider {
 
-	private final RunViewModel f_model;
+  private final RunViewModel f_model;
 
-	RunViewLabelProvider(final RunViewModel model) {
-		assert model != null;
-		f_model = model;
-	}
+  RunViewLabelProvider(final RunViewModel model) {
+    assert model != null;
+    f_model = model;
+  }
 
-	public Image getColumnImage(Object element, int columnIndex) {
-		final RunDescription rowData = (RunDescription) element;
-		final String symbolicName = f_model.getImageSymbolicName(rowData,
-				columnIndex);
-		if (symbolicName != null)
-			return SLImages.getImage(symbolicName);
-		else
-			return null;
-	}
+  public Image getColumnImage(Object element, int columnIndex) {
+    final RunDescription rowData = (RunDescription) element;
+    final String symbolicName = f_model.getImageSymbolicName(rowData, columnIndex);
+    if (symbolicName != null) {
+      if (columnIndex == 1) {
+        /*
+         * Special processing for android projects -- this is a total hack
+         */
+        String runName = f_model.getText(rowData, columnIndex);
+        if (runName != null) {
+          return SLImages.getImageForAndroidProject();
+        }
+      }
+      return SLImages.getImage(symbolicName);
+    } else
+      return null;
+  }
 
-	public String getColumnText(Object element, int columnIndex) {
-		final RunDescription rowData = (RunDescription) element;
-		return f_model.getText(rowData, columnIndex);
-	}
+  public String getColumnText(Object element, int columnIndex) {
+    final RunDescription rowData = (RunDescription) element;
+    return f_model.getText(rowData, columnIndex);
+  }
 
-	public void addListener(ILabelProviderListener listener) {
-		// ignore
-	}
+  public void addListener(ILabelProviderListener listener) {
+    // ignore
+  }
 
-	public void dispose() {
-		// nothing to do
-	}
+  public void dispose() {
+    // nothing to do
+  }
 
-	public boolean isLabelProperty(Object element, String property) {
-		return false;
-	}
+  public boolean isLabelProperty(Object element, String property) {
+    return false;
+  }
 
-	public void removeListener(ILabelProviderListener listener) {
-		// ignore
-	}
+  public void removeListener(ILabelProviderListener listener) {
+    // ignore
+  }
 }

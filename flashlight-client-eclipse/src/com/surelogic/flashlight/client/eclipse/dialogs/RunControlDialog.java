c715308422e0f4c7739c26374d594bea0ef96761
Tim Halloran <tim.halloran@surelogic.com>
2012-12-05 15:33:10 -0500

    Model added to dialog
    Former-commit-id: 4ca9c10b397c57bca025f0a32b6a234088c5c647

--- flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/dialogs/RunControlDialog.java
+++ flashlight-client-eclipse/src/com/surelogic/flashlight/client/eclipse/dialogs/RunControlDialog.java
@@ -1,5 +1,12 @@
 package com.surelogic.flashlight.client.eclipse.dialogs;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
@@ -13,6 +20,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
@@ -20,25 +28,30 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 
+import com.surelogic.NonNull;
 import com.surelogic.Nullable;
 import com.surelogic.common.CommonImages;
+import com.surelogic.common.Pair;
+import com.surelogic.common.SLUtility;
 import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.ui.EclipseUIUtility;
 import com.surelogic.common.ui.SLImages;
 import com.surelogic.flashlight.client.eclipse.Activator;
+import com.surelogic.flashlight.common.model.DataCollectingRunState;
+import com.surelogic.flashlight.common.model.IDataCollectingRun;
+import com.surelogic.flashlight.common.model.IRunControlObserver;
+import com.surelogic.flashlight.common.model.RunControlManager;
 
-public class RunControlDialog extends Dialog {
-
-  private static RunControlDialog INSTANCE = null;
+public final class RunControlDialog extends Dialog implements IRunControlObserver {
 
   public static void show() {
-    if (INSTANCE == null) {
-      INSTANCE = new RunControlDialog(EclipseUIUtility.getShell());
-    }
-    INSTANCE.open();
+    RunControlDialog dialog = new RunControlDialog(EclipseUIUtility.getShell());
+    RunControlManager.getInstance().register(dialog);
+    dialog.open();
+    RunControlManager.getInstance().remove(dialog);
   }
 
-  public RunControlDialog(Shell parentShell) {
+  private RunControlDialog(Shell parentShell) {
     super(parentShell);
     /*
      * Ensure that this dialog is modeless.
@@ -144,13 +157,13 @@ public class RunControlDialog extends Dialog {
 
   private static final String SEARCH_PROMPT = "Search...";
 
-  private final void showSearchPrompt(final Text search) {
+  private void showSearchPrompt(final Text search) {
     search.setText(SEARCH_PROMPT);
     search.setForeground(search.getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY));
     search.setData(SEARCH_PROMPT);
   }
 
-  private final void clearSearchPromptIfNecessary(final Text search) {
+  private void clearSearchPromptIfNecessary(final Text search) {
     if (search.getData() == SEARCH_PROMPT) {
       search.setText("");
       search.setForeground(null);
@@ -158,22 +171,109 @@ public class RunControlDialog extends Dialog {
     }
   }
 
-  private final void searchCleared() {
+  private void searchCleared() {
     System.out.println("searchCleared()");
   }
 
-  private final void searchFor(@Nullable String value) {
+  private void searchFor(@Nullable String value) {
     System.out.println("searchFor(" + value + ")");
 
     if (value == null || "".equals(value))
       searchCleared();
   }
 
-  private final void clearListPressed() {
+  private void clearListPressed() {
     System.out.println("clearListPressed()");
   }
 
   protected IDialogSettings getDialogBoundsSettings() {
     return Activator.getDefault().getDialogSettings();
   }
+
+  @Override
+  public void launchedRunStateChanged(final IDataCollectingRun run, final DataCollectingRunState newState) {
+    // NOT CALLED IN THE UI THREAD
+    updateModelSafe();
+  }
+
+  @Override
+  public void launchedRunCleared(Set<IDataCollectingRun> cleared) {
+    // NOT CALLED IN THE UI THREAD
+    updateModelSafe();
+  }
+
+  private static class RunModel {
+
+    RunModel(@NonNull IDataCollectingRun run, @NonNull DataCollectingRunState state) {
+      f_run = run;
+      f_state = state;
+    }
+
+    @NonNull
+    final IDataCollectingRun f_run;
+    @NonNull
+    DataCollectingRunState f_state;
+
+    @NonNull
+    String getRunLabel() {
+      return f_run.getRunSimpleNameforUI();
+    }
+
+    @NonNull
+    String getRunStateLabel() {
+      return f_state.getLabel();
+    }
+
+    @NonNull
+    String getTimeSinceLaunch() {
+      final long launched = f_run.getLaunchTime().getTime();
+      final long now = new Date().getTime();
+      final long durationMS = now - launched;
+      return SLUtility.toStringDurationS(durationMS, TimeUnit.MILLISECONDS);
+    }
+
+  }
+
+  private final ArrayList<RunModel> f_model = new ArrayList<RunModel>();
+
+  @Nullable
+  private RunModel getModelFor(IDataCollectingRun run) {
+    for (RunModel element : f_model) {
+      if (element.f_run.equals(run))
+        return element;
+    }
+    return null;
+  }
+
+  private void updateModelSafe() {
+    Display.getCurrent().asyncExec(new Runnable() {
+      public void run() {
+        updateModelInUIThreadContext();
+      }
+    });
+  }
+
+  private void updateModelInUIThreadContext() {
+    ArrayList<Pair<IDataCollectingRun, DataCollectingRunState>> runInfo = RunControlManager.getInstance().getManagedRuns();
+    List<RunModel> stillExists = new ArrayList<RunModel>();
+    for (Pair<IDataCollectingRun, DataCollectingRunState> pair : runInfo) {
+      RunModel runModel = getModelFor(pair.first());
+      if (runModel == null) {
+        // new run
+        runModel = new RunModel(pair.first(), pair.second());
+        // TODO
+      }
+      stillExists.add(runModel);
+      // update run
+      // TODO
+    }
+    for (Iterator<RunModel> iterator = f_model.iterator(); iterator.hasNext();) {
+      final RunModel runModel = iterator.next();
+      if (!stillExists.contains(runModel)) {
+        iterator.remove();
+        // TODO UI
+      }
+    }
+  }
+
 }

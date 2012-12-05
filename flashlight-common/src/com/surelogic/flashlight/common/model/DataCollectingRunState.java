c8e3930d54918cfaf77699326df5b39be2ba2d6a
Tim Halloran <tim.halloran@surelogic.com>
2012-12-05 13:50:37 -0500

    Added ui labels to enum.
    Former-commit-id: 82dac81b08d24ecd40c9f7ad4547ef1b95c5b33f

--- flashlight-common/src/com/surelogic/flashlight/common/model/DataCollectingRunState.java
+++ flashlight-common/src/com/surelogic/flashlight/common/model/DataCollectingRunState.java
@@ -1,5 +1,22 @@
 package com.surelogic.flashlight.common.model;
 
 public enum DataCollectingRunState {
-  STARTING, COLLECTING, STOP_COLLECTION_REQUESTED, FINISHED
+
+  STARTING("Launching the Flashlight-instrumented application..."),
+
+  COLLECTING("Collecting data from running application..."),
+
+  STOP_COLLECTION_REQUESTED("Data collection is in the process of shutting down..."),
+
+  FINISHED("Data collection completed");
+
+  private final String f_label;
+
+  private DataCollectingRunState(String label) {
+    f_label = label;
+  }
+
+  public String getLabel() {
+    return f_label;
+  }
 }

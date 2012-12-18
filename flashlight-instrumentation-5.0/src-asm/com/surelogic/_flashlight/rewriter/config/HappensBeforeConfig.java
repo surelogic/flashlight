e76cb2976ab845594fd81cfeef405497a211992d
Nathan Boy <nathan.boy@gmail.com>
2012-12-18 16:09:17 -0500

    Fixed bugs in getInternal* of HappensBeforeCongif.
    Former-commit-id: 352c05993688853404f17642044f9febba309d6d

--- flashlight-instrumentation-5.0/src-asm/com/surelogic/_flashlight/rewriter/config/HappensBeforeConfig.java
+++ flashlight-instrumentation-5.0/src-asm/com/surelogic/_flashlight/rewriter/config/HappensBeforeConfig.java
@@ -422,7 +422,8 @@ public class HappensBeforeConfig {
     private static final Pattern primitive = Pattern
             .compile("(byte|short|int|long|float|double|boolean|char|void)((\\s*\\[\\s*\\]\\s*)*)");
     private static final Pattern array = Pattern.compile("\\s*\\[\\s*\\]\\s*");
-    private static final Pattern type = Pattern.compile("([^\\[\\s]+)(.*)");
+    private static final Pattern type = Pattern
+            .compile("([^\\[\\s]+)((\\s*\\[\\s*\\]\\s*)*)");
     private static final Map<String, String> primMap = new HashMap<String, String>();
     static {
         primMap.put("int", "I");
@@ -460,7 +461,7 @@ public class HappensBeforeConfig {
         } else {
             m = type.matcher(jlsName);
             m.matches();
-            typeName = getInternalName(m.group(1) + ";");
+            typeName = "L" + getInternalName(m.group(1) + ";");
         }
         if (m.group(3) == null) {
             return typeName;

a03d8f0a162bae4df31670d1e4fe69093d92130a
Nathan Boy <nathan.boy@gmail.com>
2013-03-06 09:52:28 -0500

    Working version of the new deadlock detection.  Predicate not added yet.
    Former-commit-id: 0f101d181abff31f5f773099e8f0b02202e1bf9d

---
+++ flashlight-common/src/com/surelogic/flashlight/common/prep/CombinationEnumerator.java
@@ -0,0 +1,73 @@
+package com.surelogic.flashlight.common.prep;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Enumerates all possible combinations of the elements in the given list, from
+ * smallest sets to largest sets. Elements must be unique and obey equals
+ * semantics.
+ * 
+ * @author nathan
+ * 
+ * @param <E>
+ */
+abstract class CombinationEnumerator<E> {
+
+    protected final List<E> elems;
+    private int[] indexes;
+    private final int len;
+
+    CombinationEnumerator(Collection<E> elems) {
+        this.elems = new ArrayList<E>(elems);
+        len = elems.size();
+    }
+
+    public void enumerate() {
+        for (int i = 1; i <= len; i++) {
+            calculateCombinations(i);
+        }
+
+    }
+
+    private void calculateCombinations(int elements) {
+        indexes = new int[elements];
+        for (int i = 0; i < indexes.length; i++) {
+            indexes[i] = 0;
+        }
+        iterateCombinations(0, 0, elements);
+    }
+
+    /**
+     * 
+     * @param index
+     *            points into indexes
+     * @param elemsRemaining
+     *            A pointer into elems, everything to the right is still
+     *            eligible to be selected
+     * @param choicesRemaining
+     *            the number of elems that we still have to select before
+     *            calling handleEnumeration
+     */
+    private void iterateCombinations(int index, int elemsRemaining,
+            int choicesRemaining) {
+        if (choicesRemaining == 0) {
+            Set<E> set = new HashSet<E>(index);
+            for (int i = 0; i < index; i++) {
+                set.add(elems.get(indexes[i]));
+            }
+            handleEnumeration(set);
+        } else {
+            for (int i = elemsRemaining; i <= len - choicesRemaining; i++) {
+                indexes[index] = i;
+                iterateCombinations(index + 1, i + 1, choicesRemaining - 1);
+            }
+        }
+    }
+
+    abstract void handleEnumeration(Set<E> set);
+
+}
---
+++ flashlight-common/test/com/surelogic/flashlight/common/prep/CombinationEnumeratorTest.java
@@ -0,0 +1,101 @@
+package com.surelogic.flashlight.common.prep;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CombinationEnumeratorTest {
+    @Test
+    public void testSingle() {
+        new CombinationEnumerator<Integer>(Arrays.asList(new Integer[] { 1 })) {
+
+            @Override
+            void handleEnumeration(Set<Integer> set) {
+                Assert.assertEquals(Collections.singleton(1), set);
+            }
+        }.enumerate();
+    }
+
+    @Test
+    public void testTwo() {
+        new CombinationEnumerator<Integer>(
+                Arrays.asList(new Integer[] { 1, 2 })) {
+            int count;
+
+            @Override
+            void handleEnumeration(Set<Integer> set) {
+                switch (count++) {
+                case 0:
+                    Assert.assertEquals(Collections.singleton(1), set);
+                    break;
+                case 1:
+                    Assert.assertEquals(Collections.singleton(2), set);
+                    break;
+                case 2:
+                    HashSet<Integer> exp = new HashSet<Integer>();
+                    exp.add(1);
+                    exp.add(2);
+                    Assert.assertEquals(exp, set);
+                    break;
+                }
+            }
+        }.enumerate();
+    }
+
+    @Test
+    public void testThree() {
+        new CombinationEnumerator<Integer>(Arrays.asList(new Integer[] { 1, 2,
+                3 })) {
+            int count;
+
+            @Override
+            void handleEnumeration(Set<Integer> set) {
+                HashSet<Integer> exp;
+                switch (count++) {
+                case 0:
+                    Assert.assertEquals(Collections.singleton(1), set);
+                    break;
+                case 1:
+                    Assert.assertEquals(Collections.singleton(2), set);
+                    break;
+                case 2:
+                    Assert.assertEquals(Collections.singleton(3), set);
+                    break;
+                case 3:
+                    exp = new HashSet<Integer>();
+                    exp.add(1);
+                    exp.add(2);
+                    Assert.assertEquals(exp, set);
+                    break;
+                case 4:
+                    exp = new HashSet<Integer>();
+                    exp.add(1);
+                    exp.add(3);
+                    Assert.assertEquals(exp, set);
+                    break;
+                case 5:
+                    exp = new HashSet<Integer>();
+                    exp.add(2);
+                    exp.add(3);
+                    Assert.assertEquals(exp, set);
+                    break;
+                case 6:
+                    exp = new HashSet<Integer>();
+                    exp.add(1);
+                    exp.add(2);
+                    exp.add(3);
+                    Assert.assertEquals(exp, set);
+                    break;
+                default:
+                    Assert.fail("Unexpected set");
+                }
+            }
+        }.enumerate();
+    }
+
+}

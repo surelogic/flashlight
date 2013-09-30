bd113dc40d6aaedea8281025cf4025a4a221c172
Nathan Boy <nathan.boy@gmail.com>
2013-09-30 15:52:15 -0400

    Some performance and formatting improvements to the new lock info.
    Former-commit-id: 443a804f784dfd2f11007127562089cd6a285019

--- flashlight-common/src/com/surelogic/flashlight/common/prep/LockInfoPostPrep.java
+++ flashlight-common/src/com/surelogic/flashlight/common/prep/LockInfoPostPrep.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import com.surelogic.common.jdbc.ConnectionQuery;
+import com.surelogic.common.jdbc.NullRowHandler;
+import com.surelogic.common.jdbc.Query;
+import com.surelogic.common.jdbc.Queryable;
+import com.surelogic.common.jdbc.Row;
 import com.surelogic.common.jdbc.SchemaData;
 import com.surelogic.common.jobs.SLProgressMonitor;
 
@@ -17,9 +21,40 @@ public class LockInfoPostPrep implements IPostPrep {
     @Override
     public void doPostPrep(Connection c, SchemaData schema,
             SLProgressMonitor mon) throws SQLException {
-        new ConnectionQuery(c).prepared("Accesses.prep.packageAccessCounts")
-                .call();
-        new ConnectionQuery(c).prepared("Accesses.prep.lockThreadInfo").call();
-    }
+        Query q = new ConnectionQuery(c);
+        mon.subTask("Calculating lock package access counts.");
+        q.prepared("Accesses.prep.packageAccessCounts").call();
+        mon.subTaskDone();
+        mon.subTask("Collecting lock thread information.");
+        q.prepared("Accesses.prep.lockThreadInfo").call();
+        mon.subTaskDone();
+        mon.subTask("Calculating lock type access counts.");
+        final Queryable<?> updateTypes = q
+                .prepared("Accesses.prep.updateLockInfoMaxTypes");
 
+        q.prepared("Accesses.prep.selectLockInfoMaxTypes",
+                new NullRowHandler() {
+
+                    @Override
+                    protected void doHandle(Row r) {
+                        updateTypes.call(r.nextLong(), r.nextLong(),
+                                r.nextLong());
+
+                    }
+                }).call();
+        mon.subTaskDone();
+        mon.subTask("Calculating lock package access counts.");
+        final Queryable<?> updatePackages = q
+                .prepared("Accesses.prep.updateLockInfoMaxPackages");
+        q.prepared("Accesses.prep.selectLockInfoMaxPackages",
+                new NullRowHandler() {
+
+                    @Override
+                    protected void doHandle(Row r) {
+                        updatePackages.call(r.nextLong(), r.nextLong(),
+                                r.nextLong());
+                    }
+                }).call();
+        mon.subTaskDone();
+    }
 }

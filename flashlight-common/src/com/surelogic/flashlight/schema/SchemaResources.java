24317112f4d9c8d6de508f1190c086b0dbf0882f
Tim Halloran <tim.halloran@surelogic.com>
2015-07-13 14:44:45 -0400

    Fixed references to resources
    Former-commit-id: bdfae619a3463b4364fd3dbf0868e9cb6a615819

--- flashlight-common/src/com/surelogic/flashlight/schema/SchemaResources.java
+++ flashlight-common/src/com/surelogic/flashlight/schema/SchemaResources.java
@@ -1,4 +1,4 @@
-package com.surelogic.flashlight.common;
+package com.surelogic.flashlight.schema;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,30 +8,20 @@ import com.surelogic.Nullable;
 import com.surelogic.common.CommonImages;
 import com.surelogic.common.i18n.I18N;
 
-public final class LibResources {
+public final class SchemaResources {
 
   public static final String PLUGIN_ID = "com.surelogic.flashlight.common";
 
   public static final String PATH = "/lib/";
+  public static final String ADHOC_PATH = "/lib/adhoc/";
 
-  /**
-   * The name of the archive that contains the Flashlight HTML documentation
-   * Zip.
-   * <p>
-   * Within this Zip is all the Flashlight on-line documents in separate
-   * directories so that users can examine them in a browser (which is often
-   * preferred to Eclipse on-line help)..
-   */
-  public static final String HTML_DOCS_ZIP = "flashlight-html-docs.zip";
+  public static final String FLASHLIGHT_QUERIES = "default-flashlight-queries.xml";
+  public static final String FLASHLIGHT_QUERIES_PATH = ADHOC_PATH + FLASHLIGHT_QUERIES;
 
-  /**
-   * Full path to the Flashlight HTML documentation Zip within this Eclipse
-   * plugin.
-   */
-  public static final String HTML_DOCS_ZIP_PATHNAME = PATH + HTML_DOCS_ZIP;
+  public static final String QUERYDOC_IMAGE_PATH = ADHOC_PATH + "docimages/";
 
   public static InputStream getStreamFor(String pathname) throws IOException {
-    final URL url = LibResources.class.getResource(pathname);
+    final URL url = SchemaResources.class.getResource(pathname);
     if (url == null)
       throw new IOException(I18N.err(323, pathname, PLUGIN_ID));
     final InputStream is = url.openStream();
@@ -40,12 +30,12 @@ public final class LibResources {
 
   @Nullable
   public static URL getDefaultQueryUrl() {
-    return Thread.currentThread().getContextClassLoader().getResource("/lib/adhoc/default-flashlight-queries.xml");
+    return Thread.currentThread().getContextClassLoader().getResource(FLASHLIGHT_QUERIES_PATH);
   }
 
   @Nullable
   public static URL getQuerydocImageURL(String imageName) {
-    final String path = "/lib/adhoc/docimages/" + imageName;
+    final String path = QUERYDOC_IMAGE_PATH + imageName;
     final URL url = Thread.currentThread().getContextClassLoader().getResource(path);
     if (url != null) {
       return url;
